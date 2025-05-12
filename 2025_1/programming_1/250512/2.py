d952ce9f45dc761ab230301dd5e40290a5b1eb9d
Azuking69 <azuking69@hotmail.co.jp>
2025-05-12 12:09:43 +0900

    25.05.12 count1

---
+++ 2025_1/programming_1/250512/2.py
@@ -0,0 +1,8 @@
+bar = []
+bar = ""
+
+bar.append(100)
+bar.append(200)
+print(bar)
+print(bar[0], "\t", bar[1])
+print(bar[2])
---
+++ 2025_1/programming_1/250512/6.py
@@ -0,0 +1,9 @@
+bar = [10, 20, 30]
+
+# update : 특정 요소의 값을 변경하는 것
+bar[0] = 20
+
+
+foo = bar[0]
+foo = 30
+print(bar)
