9d7265867e3d6753fa6e6d74c433509980db8930
Azuking69 <azuking69@hotmail.co.jp>
2025-05-24 15:00:03 +0900

    25.05.24 count1

--- 2025_1/programming_1/250523/practice.py
+++ 2025_1/programming_1/250523/practice.py
@@ -13,22 +13,55 @@ A = int(input("시작 범위를 입력하세요: "))
 B = int(input("끝 범위를 입력하세요: "))
 
 #5 A ~ B 범위의 난수를 N개 생성하여 리스트에 저장한다.
-list = []
+num_list = []
 for val in range(N):
     num = random.randint(A, B)
-    list.append(num)
+    num_list.append(num)
 # print(list)
 
 #6 고유 숫자 리스트
-num_list = []
-for i in list:
-    if i not in num_list:
-        num_list.append(i)
+inherent_list = []
+for i in num_list:
+    if i not in inherent_list:
+        inherent_list.append(i)
     else:
         continue
-print(num_list)
+print(f"고유 숫자 리스트: {inherent_list}")
+
+#7 리스트에서 빈도수를 출력
+frequency_list = []
+for j in inherent_list:
+    frequency_list.append(num_list.count(j))
+print(f"빈도 숫 리스트: {frequency_list}\n")
+
+#8 리스트에서 가장 빈도수가 높은 숫자 3개를 찾아 출력
+top_num = []
+top_frequency = []
+
+for _ in range(3):
+    if not frequency_list:
+        break
+
+    max_value = frequency_list[0]
+    for f in frequency_list:
+        if f > max_value:
+            max_value = f
     
-#7 리스트에서 가장 빈도수가 높은 숫자 3개를 찾아 출력
+    for i in range(len(frequency_list)):
+        if frequency_list[i] == max_value and num_list[i] not in top_num:
+            top_num.append(num_list[i])
+            top_frequency.append(frequency_list[i])
 
+    # ④ 最大回数を取り除く（次の最大を探すため）
+    i = 0
+    while i < len(frequency_list):
+        if frequency_list[i] == max_value:
+            del frequency_list[i]
+            del num_list[i]
+        else:
+            i += 1
 
-#7 
+# ⑤ 出力
+print("가장 많이 등장한 숫자 Top 3 (동점 포함):")
+for i in range(len(top_num)):
+    print(f"{top_num[i]} → {top_frequency[i]}회")
