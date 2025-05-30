f26b2103105c126de67eeeb046fc27065e13d547
Azuking69 <azuking69@hotmail.co.jp>
2025-05-30 18:42:48 +0900

    25.05.30 count2

--- 2025_1/programming_1/250523/practice2.py
+++ 2025_1/programming_1/250523/practice2.py
@@ -4,7 +4,7 @@
 """
 
 #1 사용자로부터 알파벳과 숫자가 섞인 문자열을 입력받는다
-user_str = input("문제와 숫자가 섞인 문자열을 입력하세요: \n")
+user_str = input("문제와 숫자가 섞인 문자열을 입력하세요: ")
 print(f"입력 문자열: {user_str}")
 
 #2 문자열에서 숫자만 추출하여 리스트에 저장
@@ -30,4 +30,10 @@ for j in total_list:
 
 print(f"변화된 리스트 (+1/-1): {conversion_list}")
 
-#5 
+#5 합이 0이 되는 연속된 부분 수열(subarray)을 모두 찾아 출력
+print(f"합이 0인 연속 부분 수열 목록:")
+for start in range(len(conversion_list)):
+    for end in range(start + 1, len(conversion_list) + 1):
+        sub = conversion_list[start:end]
+        if sum(sub) == 0:
+            print(f"-{sub}")
