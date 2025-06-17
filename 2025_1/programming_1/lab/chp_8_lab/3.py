869886e0806834f6abb2fc4617b06676b154d437
Azuking69 <azuking69@hotmail.co.jp>
2025-06-17 23:41:12 +0900

    25.06.17 count9

--- 2025_1/programming_1/lab/chp_8_lab/3.py
+++ 2025_1/programming_1/lab/chp_8_lab/3.py
@@ -1,10 +1,9 @@
 """
 사용자로부터 이름을 입력받아 "Hello, [name]!"를 출력하는 함수 greet_user를 작성
 """
-
-#1 정의
+#1 인사 정의
 def greet_user(name):
-    return "Hello, " + name + "!"
+    print(f"Hello, {name}!")
 
-#2 Hello, Alice! 출력
-print(greet_user('Alice'))
+#2 출력: Hello, Alice!
+greet_user('Alice')
