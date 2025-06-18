e4e73fccfc5431d58d8e8acda3cb01d49f13859c
Azuking69 <azuking69@hotmail.co.jp>
2025-06-18 16:24:12 +0900

    25.06.18 count4

---
+++ 2025_1/programming_1/250618/2.py
@@ -0,0 +1,19 @@
+
+std_score = {}
+
+# 전체 학번 정보
+std_dict = {}
+
+# g학생 한 명의 정보
+std_data = {}
+
+std_score['id'] = int(input("ID를 입력하세요:\t"))
+std_score['name'] = int(input("이름을 입력하세요:\t"))
+
+#
+#
+#
+
+std_dict[std_score['id']] = std_data
+
+print(std_dict[std_data['id']]['name'])
