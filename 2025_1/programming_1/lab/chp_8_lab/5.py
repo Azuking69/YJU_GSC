81408098102677ce59c4e826b59f72876df5f8d6
Azuking69 <azuking69@hotmail.co.jp>
2025-06-18 09:49:06 +0900

    25.06.18 count1

--- 2025_1/programming_1/lab/chp_8_lab/5.py
+++ 2025_1/programming_1/lab/chp_8_lab/5.py
@@ -1,14 +1,16 @@
 """
 주어진 수가 짝수인지 홀수인지 판별하는 함수 is_even을 작성
 """
-
-#1 함수 정의
+# 함수 정의
 def is_even(num):
+    # 짝수
     if num % 2 == 0:
         return "True"
+    # 홀수
     else:
         return "False"
 
-#2 출력
+#출력: True
 print(is_even(4))
+#출력: False
 print(is_even(5))
