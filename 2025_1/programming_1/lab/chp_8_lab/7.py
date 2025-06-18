256a3ae53c4e015c44abab90c4b6906a5399d797
Azuking69 <azuking69@hotmail.co.jp>
2025-06-18 10:10:45 +0900

    25.06.18 count3

--- 2025_1/programming_1/lab/chp_8_lab/7.py
+++ 2025_1/programming_1/lab/chp_8_lab/7.py
@@ -2,17 +2,21 @@
 리스트와 타겟 숫자를 매개변수로 받아, 타겟 숫자가 리스트 내에 있는지
 여부를 반환하는 함수 contains를 작성
 """
-#1 정의
-def contains(a_list, b):
-    #2 처음 index
-    i = 0
-    while i < len(a_list):
-        if a_list[i] == b:
+# 함수 정의
+def contains(num_list, num):
+    # 처음 index
+    index = 0
+    # 검사 횟수
+    while index < len(num_list):
+        # "True"시
+        if num_list[index] == num:
             return "True"
-        #3 다음 index
-        i += 1
+        # 다음 index
+        index += 1
+    # "False"시("True"이외)
     return "False"
 
-#4 출력
+# 출력: True
 print(contains([1, 2, 3, 4], 3))
+# 출력: False
 print(contains([1, 2, 3, 4], 8))
