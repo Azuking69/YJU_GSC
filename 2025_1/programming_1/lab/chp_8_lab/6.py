4306c65fbc8eedbdb8d1f32ea8fa0a53c2f7f1eb
Azuking69 <azuking69@hotmail.co.jp>
2025-06-18 09:56:24 +0900

    25.06.18 count2

--- 2025_1/programming_1/lab/chp_8_lab/6.py
+++ 2025_1/programming_1/lab/chp_8_lab/6.py
@@ -2,10 +2,14 @@
 TML 태그 이름과 내용을 매개변수로 받아,
 해당 태그로 감싸진 HTML 요소를 반환하는 함수 wrap_in_tag를 작성
 """
-#1 출력 방식 정의
-def wrap_in_tag(a, b):
-    return (f'<{a}>{b}<{a}>')
+# 함수 정의
+def wrap_in_tag(char, content):
+    # 문서 작성
+    total = (f"<{char}>{content}<{char}>")
+    # 가져가기
+    return total
 
-#2 출력
+# 출력: <p>hello<p>
 print(wrap_in_tag('p', 'hello'))
+# 출력: <b>world<b>
 print(wrap_in_tag('b', 'world'))
