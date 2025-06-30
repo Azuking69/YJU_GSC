6514c8dcd8f917d32a83e1cdef50f2194f7bca7e
Azuking69 <azuking69@hotmail.co.jp>
2025-06-30 21:47:28 +0900

    25.06.30 count3

---
+++ 2025_1/programming_1/Special_lecture/250630/BasicTypeEX.java
@@ -0,0 +1,21 @@
+
+public class BasicTypeEX {
+
+	public static void main(String args[]) {
+		
+		// Java의 기본 자료형
+		// 변수 : 변하는 값을 저장할 수 있는 공간 
+		// Java에서는 변수를 선언할 때 변수가 어떤 값을 가질수 있는지 
+		// 저장해주어야함(변수타입을 저장해야함) 
+		
+		// 1. 정수형
+		// byte - short - int - long
+		// 2. 실수형
+		// 3. 문자형
+		// 4. boolean형
+		
+		int x = 10;
+		
+	} 
+	
+}
