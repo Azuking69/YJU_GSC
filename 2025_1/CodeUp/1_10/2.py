d77b35cb2a31349d278dd78a743e51b51b04532d
Azuking69 <azuking69@hotmail.co.jp>
2025-06-30 17:06:18 +0900

    25.06.30 count1

---
+++ 2025_1/CodeUp/1_10/2.py
@@ -0,0 +1,9 @@
+"""
+이번에는 공백( )을 포함한 문장을 출력한다.
+다음 문장을 출력해보자.
+Hello World
+"""
+word_1 = "Hello"
+word_2 = "World"
+
+print(word_1, word_2)
---
+++ 2025_1/CodeUp/1_10/5.py
@@ -0,0 +1,6 @@
+"""
+이번에는 큰따옴표(")(double quotation mark)가 포함된 출력문을 연습해보자.
+다음 문장을 출력하시오.
+"Hello World"
+"""
+print('"Hello World"')
