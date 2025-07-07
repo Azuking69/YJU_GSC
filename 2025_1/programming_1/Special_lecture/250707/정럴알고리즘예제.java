d8b9980248f2a9ea46469658fe7764c983958dd0
Azuking69 <azuking69@hotmail.co.jp>
2025-07-07 16:04:35 +0900

    25.07.07 count9

---
+++ 2025_1/programming_1/Special_lecture/250707/정럴알고리즘예제.java
@@ -0,0 +1,15 @@
+
+class Sor{
+	//두개의 정수값을 전달받아서 가장 큰값 리턴하는 메소드
+	public int max(int x, int y) {
+		
+		return 0;
+	}
+}
+
+public class 정럴알고리즘예제 {
+	public static void main(String[] args) {
+		
+	}
+
+}
