5ef08d34203d7938e2e4c47bcbcfa5ca4101814f
Azuking69 <azuking69@hotmail.co.jp>
2025-07-10 17:17:45 +0900

    25.07.10 count12

--- 2025_1/programming_1/Special_lecture/250703/Client.java
+++ 2025_1/programming_1/Special_lecture/250703/Client.java
@@ -4,12 +4,15 @@ import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
-import org.omg.CORBA.portable.OutputStream;
+import java.io.OutputStream;
 
 class MyChat extends JFrame {
 	JTextField jf;
+	String nick = "";
 	public MyChat() {
+		nick = JOptionPane.showInputDialog("닉네임을 입력해 주세요: ");
 		jf = new JTextField(25);
 		jf.addActionListener(new MyListener());
 		add(jf);
@@ -17,19 +20,19 @@ class MyChat extends JFrame {
 		setLocation(100, 100);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	class Mylistener implements ActionListener{
+	class MyListener implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
 			try {
 			Socket client = new Socket("10.30.14.160", 8888);
 			OutputStream os = client.getOutputStream();
-			String msg = jf.getText();
+			String msg = nick + ":" +  jf.getText();
 			os.write(msg.getBytes());
 			jf.setText("");
 			}catch(Exception ignore) {
 			}
-		
+		}
 	}
 }
 
@@ -37,6 +40,6 @@ public class Client {
 	public static void main(String args[])
 			throws UnknownHostException, IOException{
 		new MyChat().setVisible(true);
-
 		}
 }
+
