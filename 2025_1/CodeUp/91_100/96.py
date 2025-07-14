ea5f0c035c5efa2e9bd3d08e642d524de7c38a97
Azuking69 <azuking69@hotmail.co.jp>
2025-07-14 13:58:02 +0900

    25.07.14 count1

--- 2025_1/CodeUp/91_100/96.py
+++ 2025_1/CodeUp/91_100/96.py
@@ -3,7 +3,33 @@
 n개의 좌표를 입력받아 십(+)자 뒤집기한 결과를 출력하는 프로그램을 작성해보자.
 """
 #1 바둑알이 깔려 있는 상황이 19 * 19 크기의 정수값으로 입력
-boad = int(input("19 * 19 크기의 정수값으로 입력: "))
+boad = []
+for _ in range(19):
+    boad.append(list(map(int, input("19 * 19 크기의 정수값으로 입력: ").split())))
 
 #2 십자 뒤집기 횟수(n)가 입력
-n = 
+n = int(input("십자 뒤집기 횟수 입력: "))
+
+#3 십자 뒤집기 좌표가 횟수(n) 만큼 입력
+for _ in range(n):
+    x, y = input("좌표가 횟수 입력: ").split()
+    x = int(x) - 1
+    y = int(y) - 1
+
+    #4 십자 뒤집기 결과를 출력
+    for i in range(19):
+        #5 0 -> 1
+        if boad[i][int(y)] == 0:
+            boad[i][int(y)] = 1
+        else:
+            boad[i][int(y)] = 0
+
+        #6 1 -> 0
+        if boad[int(x)][i] == 1:
+            boad[int(x)][i] = 0
+        else:
+            boad[int(x)][i] = 1
+        
+#7 출력
+for i in range(19):
+    print(' '.join(map(str, boad[i])))
