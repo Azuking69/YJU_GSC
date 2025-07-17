51ee1dee6a9b61e6d2dbc973440bf643d3453c8b
Azuking69 <azuking69@hotmail.co.jp>
2025-07-17 15:59:14 +0900

    25.07.16 count3

---
+++ 2025_1/Special_lecture/250717/BasicTypeEx.java
@@ -0,0 +1,18 @@
+
+public class BasicTypeEx {
+	public static void main(String[] args) {
+		//정수형 : 암목적으로 int 기본
+		int x = 0;
+		
+		//실수형 : 명시적으로 default 타입 double
+		double d = 0.1;
+		float f = 0.1f;
+		
+		//문자형 : char
+		char c = 'A';
+		System.out.println(c);
+		c = 66;
+		System.out.println(c);
+		
+	}
+}
