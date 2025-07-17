7a8c0846ca7fcf763162d51f33d37dc1141e66ea
Azuking69 <azuking69@hotmail.co.jp>
2025-07-17 16:58:45 +0900

    25.07.16 count4

--- 2025_1/Special_lecture/250717/BasicTypeEx.java
+++ 2025_1/Special_lecture/250717/BasicTypeEx.java
@@ -1,12 +1,18 @@
 
 public class BasicTypeEx {
 	public static void main(String[] args) {
+		//Systemがオブジェクト、outが変数
+		System.out.println("Hello");
+		
+		
 		//정수형 : 암목적으로 int 기본
 		int x = 0;
+		
 		
 		//실수형 : 명시적으로 default 타입 double
 		double d = 0.1;
 		float f = 0.1f;
+		
 		
 		//문자형 : char
 		char c = 'A';
@@ -14,5 +20,21 @@ public class BasicTypeEx {
 		c = 66;
 		System.out.println(c);
 		
+		
+		//boolean type
+		boolean boo = true;
+		
+		String str = new String("ABC");
+		str.equals(str);
+		
+		// "="의 의미
+		int xx = 10;
+		int yy = 100;
+		//xx의 값은? => 여전히 10
+		
+		int[] x1 = {10, 20, 30};
+		int[] y1 = x1;
+		y1[0] = 100;
+		//x1[0]의 값은? => 100
 	}
 }
---
+++ 2025_1/Special_lecture/250717/SwitchEx.java
@@ -0,0 +1,36 @@
+
+public class SwitchEx {
+	public static void main(String[] args) {
+		int score = 90;
+		
+		if(score >= 90) {
+			System.out.println("A");
+		}else if (score >= 80) {
+			System.out.println("B");
+		}else if (score >= 70) {
+			System.out.println("C");
+		}else {
+			System.out.println("F");
+		}
+		
+		//switまで入力してCTRL+スペースを押すと予測出てくる
+		int r = 9;
+		switch (r) {
+		case 1:
+			System.out.println("1등");
+			break;
+		case 2:
+			System.out.println("2등");
+			break;
+		case 3:
+			System.out.println("3등");
+			break;
+		case 4:
+			System.out.println("4등");
+			break;
+		default:
+			System.out.println("탈락");
+			break;
+		}
+	}
+}
