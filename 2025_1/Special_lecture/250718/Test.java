fbc1dea642a831968d942f15171cc8c8045fb89f
Azuking69 <azuking69@hotmail.co.jp>
2025-07-18 17:24:05 +0900

    25.07.18 count5

---
+++ 2025_1/Special_lecture/250718/Test.java
@@ -0,0 +1,16 @@
+
+class MyTest extends Object{
+	public MyTest() {
+		super();
+		
+	}
+}
+
+
+public class Test {
+	public static void main(String[] args) {
+		Object o = new MyTest();
+
+	}
+	
+}
