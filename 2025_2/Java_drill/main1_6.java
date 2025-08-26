c5b96efab78f7196dd7a0c18ee2efbfd7bbac6f3
Azuki.I <azuking69@hotmail.co.jp>
2025-08-26 13:09:19 +0900

    25.08.26 count5

--- 2025_2/Java_drill/main1_6.java
+++ 2025_2/Java_drill/main1_6.java
@@ -4,6 +4,7 @@ public class main1_6 {
     public static void main(String[] args){
         // 変数の宣言
         String name = "이케다 아즈키";
+        // 出力
         System.out.println(name);
     }
 }
---
+++ 2025_2/Java_drill/main1_7.java
@@ -0,0 +1,10 @@
+package Java_drill;
+
+public class main1_7 {
+    public static void main(String[] args) {
+        // 変数の宣言
+        double pi = 3.14159;
+        // 出力
+        System.out.println(pi);
+    }
+}
