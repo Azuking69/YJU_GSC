51853b543304c0fba5b2b71d89fb89826f1401b7
Azuki.I <azuking69@hotmail.co.jp>
2025-08-26 16:49:51 +0900

    25.08.26 count14

--- 2025_2/Java_drill/main1_15.java
+++ 2025_2/Java_drill/main1_15.java
@@ -4,6 +4,8 @@ public class main1_15 {
     public static void main(String[] args) {
         // 変数 count の宣言と初期化
         int count = 0;
+        // 確認
+        System.out.println(count);
         // 値の更新
         count = 5;
         // 出力
---
+++ 2025_2/Java_drill/main1_16.java
@@ -0,0 +1,14 @@
+package Java_drill;
+
+public class main1_16 {
+    public static void main(String[] args){
+        // 変数宣言
+        String message = "Hello";
+        // 確認
+        System.out.println(message);
+        // 更新
+        message = "こんにちは";
+        // 出力
+        System.out.println(message);
+    }
+}
