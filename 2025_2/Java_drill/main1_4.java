5076b0c7344653cf18923ef37b72a3f095aa9ae7
Azuki.I <azuking69@hotmail.co.jp>
2025-08-26 12:43:19 +0900

    25.08.26 count3

---
+++ 2025_2/Java_drill/main1_4.java
@@ -0,0 +1,11 @@
+package Java_drill;
+
+public class main1_4 {
+    public static void main(String[] args){
+        int a = 5, b = 10, c = 15;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+    
+}
