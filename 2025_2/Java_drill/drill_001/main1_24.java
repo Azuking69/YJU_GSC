238a5cceeca16ff934876fe25fd4184d3fe8c1ed
Azuki.I <azuking69@hotmail.co.jp>
2025-08-26 17:44:50 +0900

    25.08.26 count21

---
+++ 2025_2/Java_drill/drill_001/main1_24.java
@@ -0,0 +1,13 @@
+package Java_drill.drill_001;
+
+public class main1_24 {
+    public static void main(String[] args) {
+        // 변수선어
+        int original = 42;
+        // 복사
+        int copy = original;
+        // 출력
+        System.out.println(original);
+        System.out.println(copy);
+    }
+}
