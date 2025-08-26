3bb911c7804277c0e07ee359c533119bb605c0e9
Azuki.I <azuking69@hotmail.co.jp>
2025-08-26 16:45:28 +0900

    25.08.26 count13

---
+++ 2025_2/Java_drill/main1_15.java
@@ -0,0 +1,12 @@
+package Java_drill;
+
+public class main1_15 {
+    public static void main(String[] args) {
+        // 変数 count の宣言と初期化
+        int count = 0;
+        // 値の更新
+        count = 5;
+        // 出力
+        System.out.println(count);
+    }
+}
