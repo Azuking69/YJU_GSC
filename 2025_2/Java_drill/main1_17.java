5c61822826decd720c18c6dd9affce3002685437
Azuki.I <azuking69@hotmail.co.jp>
2025-08-26 16:54:04 +0900

    25.08.26 count15

---
+++ 2025_2/Java_drill/main1_17.java
@@ -0,0 +1,12 @@
+package Java_drill;
+
+public class main1_17 {
+    public static void main(String[] args) {
+        // 변수 선어 / 초기화
+        int total = 10;
+        // 값 갱신(더하기)
+        total = total + 5;
+        // 출력
+        System.out.println(total);
+    }
+}
