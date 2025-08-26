a31ce398b1e870421b0d2e35cf82303d0d37a61e
Azuki.I <azuking69@hotmail.co.jp>
2025-08-26 13:49:32 +0900

    25.08.26 count12

---
+++ 2025_2/Java_drill/main1_14.java
@@ -0,0 +1,14 @@
+package Java_drill;
+
+public class main1_14 {
+    public static void main(String[] args) {
+        // 変数 a の宣言と初期化
+        int a = 5;
+        // 変数 b の宣言と初期化
+        int b;
+        b = 10;
+        // 出力
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
