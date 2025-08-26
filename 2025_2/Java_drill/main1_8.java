6711883486e6895969ecd36e1061a15fcd0a3129
Azuki.I <azuking69@hotmail.co.jp>
2025-08-26 13:12:39 +0900

    25.08.26 count6

---
+++ 2025_2/Java_drill/main1_8.java
@@ -0,0 +1,14 @@
+package Java_drill;
+
+public class main1_8 {
+    public static void main(String[] args) {
+        // 変数の宣言 / 初期値の代入
+        int count = 5;
+        // 確認
+        System.out.println(count);
+        // 再代入
+        count = 3;
+        // 出力
+        System.out.println(count);
+    }
+}
