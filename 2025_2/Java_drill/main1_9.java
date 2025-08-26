fdb1a7b5804a85427c07b66f96879364bfb3871c
Azuki.I <azuking69@hotmail.co.jp>
2025-08-26 13:20:03 +0900

    25.08.26 count7

---
+++ 2025_2/Java_drill/main1_9.java
@@ -0,0 +1,11 @@
+package Java_drill;
+
+public class main1_9 {
+    public static void main(String[] args) {
+        // 変数の宣言
+        int x = 10, y = 20;
+        // 出力
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
