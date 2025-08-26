e46f0a35872e1a2546f3d7581a2b1fecaad163a2
Azuki.I <azuking69@hotmail.co.jp>
2025-08-26 13:45:30 +0900

    25.08.26 count11

---
+++ 2025_2/Java_drill/main1_13.java
@@ -0,0 +1,10 @@
+package Java_drill;
+
+public class main1_13 {
+    public static void main(String[] args) {
+        // 定数の宣言と初期化
+        final double PI = 3.14159;
+        // 出力
+        System.out.println(PI);
+    }
+}
