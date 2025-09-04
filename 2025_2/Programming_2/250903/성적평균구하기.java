49ed4dcee98184732089dfecc46c432ac2c47cfb
Azuki.I <azuking69@hotmail.co.jp>
2025-09-04 11:59:23 +0900

    25.09.04 count1

--- 2025_2/Programming_2/250903/성적평균구하기.java
+++ 2025_2/Programming_2/250903/성적평균구하기.java
@@ -1,11 +1,20 @@
 
 class MyPerson{
-	int age = 20;
-	String name = "김길동";
-	int score1 = 90;
-	int score2 = 80;
-	int score3 = 70;
+	int age;
+	String name;
+	int score1;
+	int score2;
+	int score3;
+	
 	int e = 0; // 평균값을 저장하기 위한 변수
+	
+	public MyPerson(String n, int a, int s1, int s2, int s3) {
+		name = n;
+		age = a;
+		score1 = s1;
+		score2 = s2;
+		score3 = s3;
+	}
 	
 	public void 성적평균구하기() {
 		int result = score1 + score2 + score3;
@@ -22,19 +31,26 @@ class MyPerson{
 
 public class 성적평균구하기 {
 	public static void main(String[] args) {
-		MyPerson m1 = new MyPerson();
+		MyPerson m1 = new MyPerson("김길동", 20, 90, 80, 70);
 		m1.성적평균구하기();
 		m1.성적표출력();
 				
 		
-		MyPerson m2 = new MyPerson();
-		m2.name = "박길동";
-		m2.age = 20;
-		m2.score1 = 100;
-		m2.score2 = 100;
-		m2.score3 = 90;
-		
+		MyPerson m2 = new MyPerson("박길동", 20, 100, 100, 90);
 		m2.성적평균구하기();
 		m2.성적표출력();
+		
+		
+		MyPerson m3 = new MyPerson("홍길동", 22, 90, 80, 90);
+		m3.성적평균구하기();
+		m3.성적표출력();
+		
+		
+		// 전체 학생의 총점과 평균
+		int totalScore = m1.score1 + m1.score2 + m1.score3
+		+ m2.score1 + m2.score2 + m2.score3
+		+ m3.score1 + m3.score2 + m3.score3;
+		
+		int totalE = totalScore / 9;
 	}
 }
