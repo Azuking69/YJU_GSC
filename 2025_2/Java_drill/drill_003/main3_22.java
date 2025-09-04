591730d05a6c6ef6d77d19ca213377de6a98ae1d
Azuki.I <azuking69@hotmail.co.jp>
2025-09-04 13:49:42 +0900

    25.09.04 count7

---
+++ 2025_2/Java_drill/drill_003/main3_22.java
@@ -0,0 +1,17 @@
+package Java_drill.drill_003;
+
+import java.util.Scanner;
+
+public class main3_22 {
+    public static void main(String[] args) {
+        // 입력 준비
+        Scanner scanner = new Scanner(System.in);
+        // 입력 내용 표지
+        System.out.print("정수를 입력하세요: ");
+        // 입력 받기
+        int num = scanner.nextInt();
+
+        // 계산
+        int twoRight = num & 2;
+    }
+}
