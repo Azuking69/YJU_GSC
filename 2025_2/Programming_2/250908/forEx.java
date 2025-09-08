5ce639d4177f1ff7d5a8b53075a0e7ec774d5139
Azuki.I <azuking69@hotmail.co.jp>
2025-09-08 15:28:06 +0900

    25.09.08 count5

--- 2025_2/Programming_2/250908/forEx.java
+++ 2025_2/Programming_2/250908/forEx.java
@@ -8,25 +8,29 @@
 // *****
 
 public class forEx {
+	
+	// x = x + 1 == x + 1
+	// x++ == x + 1
+	// ++x == x = x + 1
+	public void printC(char c, int x) {
+		System.out.println(c);
+		if (x > 0) {
+			System.out.print(x);
+			printC(c, x--);
+		}
+	}
+	
 	public static void main(String[] args) {
-//		for (int i = 0; i < 10; i++) {
-//			System.out.print(i);
+		forEx f = new forEx();
+		f.printC('*', 2);
+			
+//		for (int j = 0; j < 10; j++){
+//			for (int i = 0; i < j; i++) {
+//				System.out.print("*");
+//			}
 //			System.out.println();
-			
-		for (int j = 0; j < 1; j++)	{
-			System.out.print("*");
-		}
-		System.out.println();
+//		}
 		
-		for (int k = 0; k < 2; k++)	{
-			System.out.print("*");
-		}
-		System.out.println();
-			
-		for (int l = 0; l < 3; l++) {
-			System.out.print("*");
-		}
-		System.out.println();
 		
 //		// 자바에서 반복문
 //		// 기본적인 문법
