9e229ca3990a0a31320e26a8e6896128757199e4
Azuki.I <azuking69@hotmail.co.jp>
2025-09-09 20:01:36 +0900

    25.09.09 count3

---
+++ 2025_2/Java_drill/drill_003/main3_26.java
@@ -0,0 +1,12 @@
+package Java_drill.drill_003;
+
+public class main3_26 {
+    public static void main(String[] args) {
+        // double 변수에 값을 대입
+        double value = 3.14159;
+        // int형으로 바꾸기
+        int intValue = (int) value;
+        // 출력
+        System.out.println(intValue);
+    }
+}
