6107f0d4cd11a7a9571533a9fddb64de9d2c2493
Azuki.I <azuking69@hotmail.co.jp>
2025-09-12 15:16:59 +0900

    25.09.12 count12

--- 2025_2/Java_drill/stepup_101/step01.java
+++ 2025_2/Java_drill/stepup_101/step01.java
@@ -5,11 +5,23 @@ public class step01 {
         // 배열 만들기
         int [] stocks1 = {10, 5, 3, 0, 8};
         printStockSum(stocks1);
+
+        int [] stocks2 = {0, 0, 0};
+        printStockSum(stocks2);
+
+        int [] stocks3 = {100};
+        printStockSum(stocks3);
+    }    
         
+    public static void printStockSum(int[] stocks){
         // 요소 더하기
-        for (int i = 0; i > 5; i++){
-            int printStockSum = 0;
-            printStockSum = printStockSum + stocks [i];
+        int sum = 0;
+
+        for (int i = 0; i < stocks.length; i++){
+            sum += stocks[i];
         }
+
+        // 출력
+        System.out.println(sum);
     }
 }
