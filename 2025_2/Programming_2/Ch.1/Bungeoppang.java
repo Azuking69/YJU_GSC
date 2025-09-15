9709c0dd634affd92aea807205a347a16fce6471
Azuki.I <azuking69@hotmail.co.jp>
2025-09-15 21:09:16 +0900

    25.09.15 count4

--- 2025_2/Programming_2/Ch.1/Bungeoppang.java
+++
@@ -1,9 +0,0 @@
-package 2025_2.Programming_2.Ch.1;
-
-
-
-public class Bungeoppang {
-    public static void main(String[] args) {
-        
-    }
-}
---
+++ 2025_2/Programming_2/Ch.1/BungeoppangTest.java
@@ -0,0 +1,26 @@
+package 2025_2.Programming_2.Ch.1;
+
+// 붕어빵 클래스(붕어빵을 만들기 위한 설계도)
+class Bungeoppang{
+    String filling; // 속 재료 (팥, 그림 등)
+
+    // 생성자 : 붕어빵 속을 설정
+    Bungeoppang(String filling){
+        this.filling = filling;
+    }
+
+    // 붕어빵 정보 출력
+    void printInfo(){
+        System.out.println("이 붕어빵의 속은 " + filling + "입니다.");
+    }
+}
+
+public class BungeoppangTest {
+    public static void main(String[] args) {
+        Bungeoppang redBean = new Bungeoppang("팥"); // 팥 붕어빵 생성
+        Bungeoppang cream = new Bungeoppang("크림"); // 크림 붕어빵 생성
+
+        redBean.printInfo(); // "이 붕어빵의 속은 팥입니다."
+        cream.printInfo(); // "이 붕어빵의 속은 크림입니다."
+    }
+}
