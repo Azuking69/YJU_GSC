540aab5b006ecb999ee50b5481a5cf5eac5a71d4
Azuki.I <azuking69@hotmail.co.jp>
2025-09-15 15:40:32 +0900

    25.09.15 count2

--- 2025_2/Programming_2/250915/AbstractEx.java
+++ 2025_2/Programming_2/250915/AbstractEx.java
@@ -1,20 +1,34 @@
 
-class Cat{
-	public void cry() {
-		System.out.println("야옹");
+abstract class Animal{
+	abstract public void cry() {
+		System.out.println("동물이 울기전에 띠링 소리");
 	}
 }
 
-class Dog{
-	public void cry() {
+class Cat extends Animal{
+	public void cry() { // 상속받은 메소드 기능재정의 -> 메소드 오바라이딩
+		System.out.println("아옹");
+	}
+}
+
+abstract class Dog extends Animal{
+	public void cry() { // 상속받은 추상화 메소드 오바라이딩해서 일반
 		System.out.println("멍멍");
 	}
 }
 
-public class AbstractEx {
+class Pig{
+	public void cry() {
+		System.out.println("꼴꼴");
+
+	}
+}
+
+public static class AbstractEx {
 	public static void main(String[] args) {
 		Cat c = new Cat();
-        c.cry();
-
+		c.cry();
+		Dog d = new Dog();
+		d.cry();
 	}
 }
