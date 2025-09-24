7a5326d336d8839577f1a40b0bc6868a23a2858e
Azuki.I <azuking69@hotmail.co.jp>
2025-09-24 15:38:43 +0900

    25.09.24 count9

--- 2025_2/Programming_2/250917/CalUI.java
+++ 2025_2/Programming_2/250917/CalUI.java
@@ -7,7 +7,6 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 
-
 // 버튼 클릭 이벤트 처리를 위한 리스너
 class MyListener implements ActionListener {
 	JTextField jf;
@@ -31,20 +30,25 @@ class MyListener implements ActionListener {
 		case "7":
 		case "8":
 		case "9":
+			
 		case "*":
 		case "/":
 		case "+":
 		case "-":
-			jf.setText(str);
+			jf.setText(jf.getText() + str);
 			break;
-            
+			
+		case "=":
+			CalLogic c = new CalLogic();
+			int result = c.doIt(jf.getText());
+			jf.setText(result + "");
+			break;
+			
 		case "C":
 			jf.setText("");
 			break;
-
-		case "=":
-			jf.setText("= 눌림");
-			break;
+		
+			
 		}
 		
 		System.out.println(str);
@@ -62,17 +66,28 @@ public class CalUI extends JFrame {
 	JButton[] b; // 0~9까지 숫자버튼
 	JButton clear; // "C" 버튼
 	JButton equal; // "=" 바튼
-	// 시작연산 버튼
+	JButton add, sub, mul, div;	// 시작연산 버튼
+	
+	
+	// class MyInnerListener implements ActionListener{
+	// 	@Override
+	// 	public void actionPerformed(ActionEvent e) {
+	// 		String t = new e.getActionCommand();
+	// 		// 내부 믈래스에서는 외부 믈래스 멤버변수 접근이 가능하다.
+	// 		jf.setText(t);
+	// 	}
+	// }
 	
 	public CalUI() {
 		super("계산기"); // 판때기 이름 설정
 		FlowLayout layout = new FlowLayout(); // 배치관리자 객체화
 		setLayout(layout);  // 판때기에 
 		
+		jf = new JTextField(15);
+		
 		// 버튼 클릭 감시자 생성
 		MyListener m = new MyListener(jf);
 		
-		jf = new JTextField(15);
 		this.add(jf); // 판때기에 입력창 추가
 		b = new JButton[10];
 		for(int i = 0; i < 10; i++) {
@@ -81,23 +96,38 @@ public class CalUI extends JFrame {
 			b[i].addActionListener(m); // 숫자버튼에 감시자
 		}
 		
-		clear = new JButton("C");
-		clear.setBackground(Color.red);
-		add(clear);
-		clear.addActionListener(m);
-
+		
+		
+		// 연산자 버튼
+		add = new JButton("+");
+		sub = new JButton("-");
+		mul = new JButton("*");
+		div = new JButton("/");
 		equal = new JButton("=");
+		
+		add(add);
+		add(sub);
+		add(mul);
+		add(div);
 		add(equal);
+		
+		add.addActionListener(m);
+		sub.addActionListener(m);
+		mul.addActionListener(m);
+		div.addActionListener(m);
 		equal.addActionListener(m);
 		
+		// "C" 버튼
+		clear = new JButton("C");
+		add(clear);
+		clear.addActionListener(m);
+		clear.setBackground(Color.red);
+		
+			
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(350, 350);
 		setLocation(250, 250);
 		setVisible(true);
-	}
-
-	public static void main(String[] args) {
-		new CalUI();
+		
 	}
 }
-
---
+++ 2025_2/Programming_2/250924/InnerClassEx.java
@@ -0,0 +1,17 @@
+
+class Outer{
+	int x = 10;
+	
+	class inner{ // 내부 클래스
+		public void printX() {
+			System.out.println(x);
+		}
+	}
+}
+
+public class InnerClassEx {
+	public static void main(String[] args) {
+		
+
+	}
+}
