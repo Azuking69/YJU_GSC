2ae7d9147043f5ed62efe9fda19620af8fb1ca94
Azuki.I <azuking69@hotmail.co.jp>
2025-09-29 13:24:28 +0900

    25.09.29 count1

---
+++ 2025_2/Programming_2/250929/IOEx.java
@@ -0,0 +1,11 @@
+import java.io.FileOutputStream;
+
+public class IOEx {
+	public static void main(String[] args) {
+		// 1) byte stream class 사용
+		// 파일에 데이터 쓰기
+		FileOutputStream fos 
+		= new FileOutputStream("c://javatest/text.txt");
+
+	}
+}
