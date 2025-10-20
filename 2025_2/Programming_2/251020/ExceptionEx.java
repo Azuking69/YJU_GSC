c0e15d534d40e56a93b15fddeff3566dd0d0d33c
Azuki.I <azuking69@hotmail.co.jp>
2025-10-20 14:19:58 +0900

    25.10.20 count3

--- 2025_2/Programming_2/251020/ExceptionEx.java
+++ 2025_2/Programming_2/251020/ExceptionEx.java
@@ -1,19 +1,21 @@
 
 public class ExceptionEx {
-
 	public static void main(String[] args) {
 		System.out.println("start");
 		int x = 10;
 		int y = 0;
 		int r = 0;
+		
 		try {
 			r = x / y;
+			System.out.println(r); // r = x / y;에 오류가 없으면 출력
+			
 		} catch (ArithmeticException e) {
 			// 예외를 잡아냈을 때 대용코드
 			System.out.println("산술연산 오류 발생");
 		}
 		
-		System.out.println(r);
+		
 		System.out.println("end");
 	}
 }
