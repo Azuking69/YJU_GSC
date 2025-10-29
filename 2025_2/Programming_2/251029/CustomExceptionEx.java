1be6bf3e3332fc34476057ecef9ae95641901f49
Azuki.I <azuking69@hotmail.co.jp>
2025-10-29 13:34:45 +0900

    25.10.29 count3

--- 2025_2/Programming_2/251029/CustomExceptionEx.java
+++ 2025_2/Programming_2/251029/CustomExceptionEx.java
@@ -1,5 +1,4 @@
 
-
 // 프로적트 진행하다 보면 현재 프로잭트 정책상
 // 예외 취급해야 되는 특정 상황 / 특정 코드가 생기게 됨
 // 이런 우리만의 예외 상황을 관리하기 위해 우리만의 커스텀된 예외 클래스를 정의해 줄 수 있다
@@ -7,22 +6,29 @@
 // 우리만의 예외를 정의해 줄 때에는 잡바의 룰에 따라 모든 예외 처리에 관련된 클래스의 
 // 최상위 클래스에 해당하는 Exception 클래스를 상속 받도록 해야 함!
 class MyExceotipn extends Exception{
-	
+	public MyExceotipn() {}
+	public MyExceotipn(String str) {
+		super(str);
+	}
 }
 
 
 public class CustomExceptionEx {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MyExceotipn {
 		// 자 그럼 우리만의 예외 처리 클래스를 실제 사용해 보자
 		try {
 			int x = 10;
-			int y = 0;
+			int y = 2;
+			if (y == 2) {
+				throw new MyExceotipn();
+			}	
 			int r = x / y;
 			System.out.println(r);
 		} catch (Exception e) {
 			System.out.println("문제 발생");
 		}
 		
-
+		throw new MyExceotipn();
+		
 	}
 }
