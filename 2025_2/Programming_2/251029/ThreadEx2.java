9f8fd2bb44048a251b61f480c4ea001ce1c16f64
Azuki.I <azuking69@hotmail.co.jp>
2025-10-29 14:22:55 +0900

    25.10.29 count6

---
+++ 2025_2/Programming_2/251029/ThreadEx2.java
@@ -0,0 +1,13 @@
+
+public class ThreadEx2 {
+	public static void main(String[] args) {
+		Car car = new Car();
+		Producer producer = new Producer(c);
+		Thread tProducer = new Thread(producer);
+		Customer customer = new Customer(c);
+		Thread tCustomer = new Thread(customer);
+		tProducer.start();
+		tCustomer.start();
+	}
+
+}
