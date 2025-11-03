bb008402adcff6ee0bb73258efa84781d7d2b86e
Azuki.I <azuking69@hotmail.co.jp>
2025-11-03 14:31:53 +0900

    25.11.03 count6

--- 2025_2/Programming_2/251103/Server.java
+++ 2025_2/Programming_2/251103/Server.java
@@ -5,6 +5,7 @@
 // : 클라이언트 수 + 1 개의 Thread 필요
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,10 +13,14 @@ public class Server {
 	public static void main(String[] args) throws IOException {
 		// 1) 클라이언트 접속 대기
 		ServerSocket ss = new ServerSocket(8888);
+		while (true) { // 여러 클라이언트 접속을 받아주기 위해 반복문 사용
 		// 클라이언트 접속대기 + 접속하면 종이컵 만들어주기
-		Socket server = ss.accept(); 
-
-		// 2) 접속한 모든 클라이언트로 부터 메시지 수신
+			Socket server = ss.accept(); 
+			
+			// 2) 접속한 모든 클라이언트로 부터 메시지 수신
+			InputStream is = server.getInputStream(); // 종이컵에서 읽기 위한 실 뽑아내기
+		}
+		
 		
 		
 		
