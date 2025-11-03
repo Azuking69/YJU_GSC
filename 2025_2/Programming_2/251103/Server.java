94cf5fd306414b80386019cc06e8481f50134b41
Azuki.I <azuking69@hotmail.co.jp>
2025-11-03 14:17:34 +0900

    25.11.03 count1

---
+++ 2025_2/Programming_2/251103/Server.java
@@ -0,0 +1,13 @@
+
+// 서버파트 구현 목록
+// 1) 클라이언트 접속 대기 → 접속 후 클라이언트와 매칭되는 종이컵(Socket) 생성
+// 2) 접속한 모든 클라이언트로 부터 메시지 수신 → 수신된 메시지를 모든 클라이언트로 송신
+// : 클라이언트 수 + 1 개의 Thread 필요
+public class Server {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
