ed3896b0458a36f52e81cc582fcbcf615cf6059e
Azuki.I <azuking69@hotmail.co.jp>
2025-11-05 14:38:39 +0900

    25.11.05 count14

--- 2025_2/Programming_2/251105/Client.java
+++ 2025_2/Programming_2/251105/Client.java
@@ -4,14 +4,19 @@
 // 3) 서버에서 보내는 메시지 수신
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Client {
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		// 1) 서버에 접속(Socket 생성)
-
 		Socket client = new Socket("127.0.0.1", 8888);
-
+		
+		// 2) 서버로 메시지 승신
+		OutputStream os = client.getOutputStream();
+		os.write();
 	}
 }
+
+
