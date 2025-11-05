d58f759d1eefd8ed97f07b82d41d4af9afe2f01a
Azuki.I <azuking69@hotmail.co.jp>
2025-11-05 14:22:02 +0900

    25.11.05 count9

--- 2025_2/Programming_2/251105/Server.java
+++ 2025_2/Programming_2/251105/Server.java
@@ -32,7 +32,8 @@ class ServerThread extends Thread{
 			// 종이컵 저장 공간에서 종이컵을 하나씩 가져와서 해당 클라이언트로 메시지 전송
 			// 그렇다는 애기는 어딘가에 여태껏 만들어진 종이컵이 다 저장되어 있는곳이 있어야 한다.
 						
-			
+			// 종이컵 저장 공간(totalSocket)에서 종이컵 하나씩 가져 와서
+			// 쓰기 위한 실(OutputStream) 뽑아내서 데이터 전송(write)
 			for (int i = 0; i < Server.totalSocket.size(); i++) {
 //				OutputStream os = Server.totalSocket.get(i).getOutputStream();
 //				os.write(b); // 방금전에 1024바이트 만큼 읽은 데이터를 전송
