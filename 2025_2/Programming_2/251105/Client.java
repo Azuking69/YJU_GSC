06f30acb885e0cb7a9a7021f0e27b5da3b387bf8
Azuki.I <azuking69@hotmail.co.jp>
2025-11-05 14:35:24 +0900

    25.11.05 count12

--- 2025_2/Programming_2/251105/Client.java
+++ 2025_2/Programming_2/251105/Client.java
@@ -3,9 +3,13 @@
 // 2) 서버로 메시지 승신
 // 3) 서버에서 보내는 메시지 수신
 
+import java.net.Socket;
+
 public class Client {
 	public static void main(String[] args) {
-		
+		// 1) 서버에 접속(Socket 생성)
+
+		Socket client = new Socket("127.0.0.1", 8888);
 
 	}
 }
