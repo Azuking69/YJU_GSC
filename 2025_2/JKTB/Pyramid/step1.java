411030148251900163459324856b739e08145e51
Azuki.I <azuking69@hotmail.co.jp>
2025-11-18 13:52:12 +0900

    25.11.18 count2

--- 2025_2/JKTB/Pyramid/step1.java
+++ 2025_2/JKTB/Pyramid/step1.java
@@ -1,5 +1,19 @@
 package JKTB.Pyramid;
 
+// #を5つ横に並べる
+// 出力例：#####
+// ・#を5個出力したい
+// ・1個ずつループで出せばよさそう
+// ・1回出力するたびに、次の#を出す
+// ・改行はいらない（1行で終わらせたい）
+
+
 public class step1 {
-    
+    public static void main(String[] args) {
+        // #を5回出力する
+        for (int i = 0; i < 5; i++){
+            // 改行しないで出力する
+            System.out.print("#");
+        }
+    }
 }
