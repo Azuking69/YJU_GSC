667c0175083a0f5cfae8c839d74dacc912c730ad
Azuki.I <azuking69@hotmail.co.jp>
2025-11-18 13:59:28 +0900

    25.11.18 count3

---
+++ 2025_2/JKTB/Pyramid/step2.java
@@ -0,0 +1,22 @@
+package JKTB.Pyramid;
+
+// // 以下のように表示されること
+// #####
+// #####
+// #####
+// #####
+// #####
+
+public class step2 {
+    public static void main(String[] args) {
+        // 행
+        for(int i = 0; i < 5; i++){
+            // 열
+            for(int j = 0; j < 5; j++){
+                System.out.print("#");
+            }
+            // そのあと改行する
+            System.out.println();
+        }
+    }
+}
