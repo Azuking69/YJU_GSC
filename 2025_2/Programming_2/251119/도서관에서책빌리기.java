05c384459916f2f364a23e008d4ad1592b84b66b
Azuki.I <azuking69@hotmail.co.jp>
2025-11-19 14:19:23 +0900

    25.11.19 count2

---
+++ 2025_2/Programming_2/251119/도서관에서책빌리기.java
@@ -0,0 +1,26 @@
+
+
+// 학생이 도서관에서 책을 빌리는 기능을 구현하다고 샌각하고
+// 관련 믈래스외 메소드를 정의후에 사용
+class 학생{
+	public void 학생이도서관에서책빌리기(){
+		
+	}
+}
+
+
+class 도서관{
+	책[] books;
+}
+
+
+class 책{
+	
+}
+
+
+public class 도서관에서책빌리기 {
+	public static void main(String[] args) {
+		
+	}
+}
