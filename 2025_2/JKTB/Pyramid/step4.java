becf86040ce6b83506f1f786da4a4e1048a6bb8f
Azuki.I <azuking69@hotmail.co.jp>
2025-11-19 19:59:26 +0900

    25.11.19 count8

---
+++ 2025_2/JKTB/Pyramid/step4.java
@@ -0,0 +1,15 @@
+package JKTB.Pyramid;
+
+// ピラミッド形に出力せよ
+//         #
+//       ###
+//     #####
+//   #######
+// #########
+
+public class step4 {
+    public static void main(String[] args) {
+        // 5行繰り返す
+        for
+    }
+}
