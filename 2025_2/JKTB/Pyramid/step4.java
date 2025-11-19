8d393658d52b0850af1163ce5a6f9bd570decee6
Azuki.I <azuking69@hotmail.co.jp>
2025-11-19 20:29:42 +0900

    25.11.19 count9

--- 2025_2/JKTB/Pyramid/step4.java
+++ 2025_2/JKTB/Pyramid/step4.java
@@ -9,7 +9,21 @@ package JKTB.Pyramid;
 
 public class step4 {
     public static void main(String[] args) {
+        // 5行を定義
+        int num = 5;
         // 5行繰り返す
-        for
+        for(int i = 1; i <= num; i++){
+            // 各行のはじめに空白を (5(num) - i) 個出力する
+            for(int j = 0; j < num - i; j++){
+                System.out.print(" ");
+            }
+
+            for(int k = 0; k < 2 * i - 1; k++){
+                System.out.print("#");
+            }
+
+            // 改行する
+            System.out.println();
+        }
     }
 }
