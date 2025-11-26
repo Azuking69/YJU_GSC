004a8dd0569094558e25b35cc0f7324e3c166d81
Azuki.I <azuking69@hotmail.co.jp>
2025-11-26 15:23:17 +0900

    25.11.26 count8

---
+++ 2025_2/Programming_2/251126/그림판.java
@@ -0,0 +1,26 @@
+
+class 삼각형{
+	public void 그리기() {
+		System.out.println("삼각형 그려지고 있다.");
+	}
+}
+
+class 동그라미{
+	public void 그리기() {
+		System.out.println("동그라미가 그려지고 있다.");
+	}
+}
+
+class 네모{
+	public void 그리기() {
+		System.out.println("네모가 그려지고 있다.");
+	}
+}
+
+
+public class 그림판 {
+	public static void main(String[] args) {
+		
+
+	}
+}
