a25e16662d60bd2c26a47022bd8b51c66a96367a
Azuki.I <azuking69@hotmail.co.jp>
2026-01-22 11:51:17 +0900

    26.01.22 count7

--- 2025_2/course-js/tests/test1.js
+++ 2025_2/course-js/tests/test1.js
@@ -1,18 +1,11 @@
-let x = 1;
+function bar() {
+  let x = 1;
+}
 
-function a_1() {
+function foo() {
   let y = 2;
-  function a_2() {
+
+  function pos() {
     let z = 3;
-    y++;
-    console.log(x, y, z);
   }
-  a_2();
 }
-
-function b_1() {
-  a_1(); // a_2() -> 1, 3, 3
-  a_1(); // a_2() -> 1, 3, 3
-}
-
-b_1();
