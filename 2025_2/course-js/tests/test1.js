8fc3ebe73491d8c06793e3364f349788b3f26fb0
Azuki.I <azuking69@hotmail.co.jp>
2026-01-23 10:25:22 +0900

    26.01.23 count3

--- 2025_2/course-js/tests/test1.js
+++ 2025_2/course-js/tests/test1.js
@@ -1,3 +1,11 @@
+// Java
+int a = 2; // a -> int, static 
+a = 2; // 오류
+
+// JS or Python
+let a = 2; // JS
+a = 2; // Python
+
 class Bar {
   constructor(argName = "gsc") {
     this.name = argName;
