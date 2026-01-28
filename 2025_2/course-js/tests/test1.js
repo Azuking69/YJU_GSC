78f37e0624f0150fdca8aed95950ee6644aa6da7
Azuki.I <azuking69@hotmail.co.jp>
2026-01-28 10:17:14 +0900

    26.01.28 count1

--- 2025_2/course-js/tests/test1.js
+++ 2025_2/course-js/tests/test1.js
@@ -1,13 +1,7 @@
-const fn1 = function () {
-  return x + y;
-};
-const fn2 = () => {
-  return x + y;
-};
-
-fn1(1, 2);
-fn2(3, 3);
-
 console.log(function () {
-  return x + y;
+  return x + yield;
 });
+
+// Arrow function
+// 1) Readable
+// 2) this binding -> lexical scope
