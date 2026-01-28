a25c82ce0dea7186e1a77aaf2bd6369f082f2e67
Azuki.I <azuking69@hotmail.co.jp>
2026-01-28 10:44:33 +0900

    26.01.28 count3

--- 2025_2/course-js/tests/test1.js
+++ 2025_2/course-js/tests/test1.js
@@ -1,6 +1,9 @@
 class Student {
   age = 20;
-  prtAge = function () {
+  prtAge = function (argAge) {
+    console.log(this.age);
+  };
+  prtAge2 = (argAge) => {
     console.log(this.age);
   };
 }
@@ -12,6 +15,9 @@ class Button {
   onclick = undefined;
 }
 let btn = new Button();
-btn.onclick = std1.prtAge;
+// btn.onclick = std1.prtAge;
 // btn.onclick();
-btn.onclick.call(std1);
+// btn.onclick.call(std1); // This brolen -> call, bind
+
+btn.onclick = std1.prtAge2;
+btn.onclick();
