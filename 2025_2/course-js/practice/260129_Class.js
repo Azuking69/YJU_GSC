2122cd61cd1ef72b6096aac36dcec10d127fdd4a
Azuki.I <azuking69@hotmail.co.jp>
2026-01-29 12:33:14 +0900

    26.01.29 count3

---
+++ 2025_2/course-js/practice/260129_Class.js
@@ -0,0 +1,11 @@
+class Student {
+  // 1) Instance Member variable
+  // 2) Instance Member method
+  // 3) Class Member variable
+  // 4) Class Member method
+
+  // Constructor
+  constructor() {}
+}
+
+let std1 = new Student();
