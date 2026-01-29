4d3eb615753ec6b0dc7e4ef1aa6024c00c2abec1
Azuki.I <azuking69@hotmail.co.jp>
2026-01-29 12:44:05 +0900

    26.01.29 count4

--- 2025_2/course-js/practice/260129_Class.js
+++ 2025_2/course-js/practice/260129_Class.js
@@ -1,11 +1,17 @@
 class Student {
-  // 1) Instance Member variable
   // 2) Instance Member method
   // 3) Class Member variable
   // 4) Class Member method
 
   // Constructor
-  constructor() {}
+  constructor(argAge) {
+    // 1) Instance Member variable
+    this.name = argAge;
+  }
 }
 
-let std1 = new Student();
+let std1 = new Student("kim");
+let std2 = new Student("lee");
+
+// 출력: kim lee
+console.log(std1.name, std2.name);
