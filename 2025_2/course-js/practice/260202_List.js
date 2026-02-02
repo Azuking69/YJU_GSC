51211fc55b9bedb75a5d193bbf95140627a6ac61
Azuki.I <azuking69@hotmail.co.jp>
2026-02-02 12:37:46 +0900

    26.02.02 count10

--- 2025_2/course-js/practice/260202_List.js
+++ 2025_2/course-js/practice/260202_List.js
@@ -2,14 +2,18 @@ print = console.log;
 
 let myList1 = [100, 200, 300]; // dense array
 let myList2 = [10, 20, 30]; // dense attay
+
 myList1[100] = 400; // space array
 
-for (let i = 0; i < myList1.length; i++) {
-  print(myList1[i]);
-}
-for (let i = 0; i < myList2.length; i++) {
-  print(myList2[i]);
-}
+print(myList1.length === Object.keys(myList1).length);
+print(myList2.length === Object.keys(myList2).length);
 
-print(`myList1: ${myList1}, length: ${myList1.length}`);
-print(`myList2: ${myList2}, length: ${myList2.length}`);
+// for (let i = 0; i < myList1.length; i++) {
+//   print(myList1[i]);
+// }
+// for (let i = 0; i < myList2.length; i++) {
+//   print(myList2[i]);
+// }
+
+// print(`myList1: ${myList1}, length: ${myList1.length}`);
+// print(`myList2: ${myList2}, length: ${myList2.length}`);
