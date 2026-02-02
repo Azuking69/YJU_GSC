e00ea84be092471c54755ac3bd57a4ecad1e71a0
Azuki.I <azuking69@hotmail.co.jp>
2026-02-02 11:59:03 +0900

    26.02.02 count8

--- 2025_2/course-js/practice/260202_List.js
+++ 2025_2/course-js/practice/260202_List.js
@@ -2,8 +2,8 @@ print = console.log;
 
 let myList1 = [100, 200, 300]; // dense array
 let myList2 = [10, 20, 30]; // dense attay
-delete myList1[1];
-delete myList2[0];
+myList1[100] = 400; // space array
+delete myList2[0]; // space array
 
 print(`myList1: ${myList1}, length: ${myList1.length}`);
 print(`myList2: ${myList2}, length: ${myList2.length}`);
