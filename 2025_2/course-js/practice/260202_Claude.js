047f1e560962bee31f532584d6e1e4ffa9fc7bdf
Azuki.I <azuking69@hotmail.co.jp>
2026-02-02 10:45:39 +0900

    26.02.02 count3

--- 2025_2/course-js/practice/260202_Claude.js
+++ 2025_2/course-js/practice/260202_Claude.js
@@ -1,9 +1,7 @@
 let myList = [10, 20, 30];
 
-for (value of myList) {
-  console.log(value);
-}
+myList[3] = 40;
 
-for (let i = 1; i < myList; i++) {
-  console.log(myList[j]);
+for (let i = 1; i < myList.length; i++) {
+  console.log(myList[i]); // myList[index]
 }
