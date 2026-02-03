30a7f0319cbd2351f3f8019722619592c4fd198c
Azuki.I <azuking69@hotmail.co.jp>
2026-02-03 12:44:04 +0900

    26.02.03 count7

--- 2025_2/course-js/practice/260203_Highrt-order.js
+++ 2025_2/course-js/practice/260203_Highrt-order.js
@@ -1,54 +1,5 @@
-// Higher-order function
-// input: fn
-// output: fn
-// Pros: Abstract and Resuable
-print = console.log;
+let myList = [10, 200, 30, 400];
 
-function forEach(argArray, argFn) {
-  for (let i = 0; i < argArray.length; i++) {
-    // print(argArray[i]);
-    argFn(argArray[i]);
-  }
-}
-
-function map(argArray, argFn) {
-  for (let i in argArray) {
-    argArray[i] = argFn(argArray[i]);
-  }
-}
-
-let myList = [10, 20, 30, 40];
-
-// 1) 현제 배열 값을 화면에 출력
-// 10, 20, 30, 40
-forEach(myList, (v) => process.stdout.write(`${v}\t`));
-// myList.forEach((v) => process.stdout.write(`${v}\t`));
-
-// 2) 각 배열의 원소에 1을 더하다
-// 요소 값이 변경 11, 21, 31, 41
-// map(myList, 1);
-// map(myList, (v) => {
-//   return v + 1;
-// });
-myList.map((v) => {
-  return v + 1;
-});
-
-// 3) 현제 배열 값을 화면에 출력
-// 11, 21, 31, 41
-forEach(myList, (v) => {
-  process.stdout.write(`${v}\t`);
-});
-
-// 4) 각 배열의 원소에 2을 더하다
-// 요소 값이 변경 13, 23, 33, 43
-// map(myList, 2);
-map(myList, (v) => {
-  return v + 2;
-});
-
-// 5) 현제 배열 값을 화면에 출력
-// 13, 23, 33, 43
-forEach(myList, (v) => {
-  process.stdout.write(`${v}\t`);
-});
+// 100이상 값들만 리스토로 생성
+newList = myList.filter((v) => v >= 100);
+console.log(newList);
