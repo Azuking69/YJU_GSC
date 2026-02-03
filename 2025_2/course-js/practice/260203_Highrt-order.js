12a805ba92c29d45b0ae42b8c78b8de7fb32bf95
Azuki.I <azuking69@hotmail.co.jp>
2026-02-03 12:51:25 +0900

    26.02.03 count8

--- 2025_2/course-js/practice/260203_Highrt-order.js
+++ 2025_2/course-js/practice/260203_Highrt-order.js
@@ -1,5 +1,23 @@
 let myList = [10, 200, 30, 400];
 
+// 배열의 각 요소를 순회
+// 어떤 조건을 만족하는 값만 새로운 배열으로 리턴
+function filter(argList, argFn) {
+  // 새로운 배열을 하나 생성
+  let newList = [];
+
+  // 입력 배열의 요소를 순회
+  for (let value of argList) {
+    // 어떤 조건을 만족하면 새로운 배열에 삽입
+    if (argFn(value)) {
+      newList.push();
+    }
+  }
+
+  // 새로운 배열을 반환
+  return newList;
+}
+
 // 100이상 값들만 리스토로 생성
 newList = myList.filter((v) => v >= 100);
 console.log(newList);
