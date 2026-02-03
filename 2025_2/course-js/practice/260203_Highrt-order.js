cfd94d146d320d578b7218c1e7f1e1b39e27d208
Azuki.I <azuking69@hotmail.co.jp>
2026-02-03 11:42:25 +0900

    26.02.03 count4

--- 2025_2/course-js/practice/260203_Highrt-order.js
+++ 2025_2/course-js/practice/260203_Highrt-order.js
@@ -2,45 +2,45 @@
 // input: fn
 // output: fn
 // Pros: Abstract and Resuable
+print = console.log;
+
+function forEach(argArray, argFn) {
+  for (let i = 0; i < argArray.length; i++) {
+    // print(argArray[i]);
+    argFn(argArray[i]);
+  }
+}
+
+function map(argArray, argAddValue) {
+  for (let i in argArray) {
+    argArray[i] += argAddValue;
+  }
+}
 
 let myList = [10, 20, 30, 40];
 
 // 1) 현제 배열 값을 화면에 출력
 // 10, 20, 30, 40
-print = console.log;
-
-for (let i = 0; i < myList.length; i++) {
-  print(myList[i]);
-}
+forEach(myList, (v) => {
+  process.stdout.write(`${v}\t`);
+});
 
 // 2) 각 배열의 원소에 1을 더하다
 // 요소 값이 변경 11, 21, 31, 41
-for (let i = 0; i < myList.length; i++) {
-  myList[i] = myList[i] + 1;
-}
-// myList[0] = myList[0] + 1;
-// myList[1] = myList[1] + 1;
-// myList[2] = myList[2] + 1;
-// myList[3] = myList[3] + 1;
+map(myList, 1);
 
 // 3) 현제 배열 값을 화면에 출력
 // 11, 21, 31, 41
-for (let i in myList) {
-  print(myList[i]);
-}
+forEach(myList, (v) => {
+  process.stdout.write(`${v}\t`);
+});
 
 // 4) 각 배열의 원소에 2을 더하다
 // 요소 값이 변경 13, 23, 33, 43
-for (let i = 0; i < myList.length; i++) {
-  myList[i] = myList[i] + 2;
-}
-// myList[0] = myList[0] + 2;
-// myList[1] = myList[1] + 2;
-// myList[2] = myList[2] + 2;
-// myList[3] = myList[3] + 2;
+map(myList, 2);
 
 // 5) 현제 배열 값을 화면에 출력
 // 13, 23, 33, 43
-for (let i in myList) {
-  print(myList[i]);
-}
+forEach(myList, (v) => {
+  process.stdout.write(`${v}\t`);
+});
