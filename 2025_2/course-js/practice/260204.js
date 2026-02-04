47c58a42e5a40023f918372206f5546747511a9a
Azuki.I <azuking69@hotmail.co.jp>
2026-02-04 12:13:22 +0900

    26.02.04 count18

--- 2025_2/course-js/practice/260204.js
+++ 2025_2/course-js/practice/260204.js
@@ -1,8 +1,5 @@
 let myList = [10.3333, 20.22, 30.333];
 
-let newList = myList.map((v) => parseInt(v));
+let newList = myList.filter((v) => v >= 15);
 
-myList.forEach((v) => {
-  console.log("-----------");
-  console.log(v);
-});
+console.log(newList);
