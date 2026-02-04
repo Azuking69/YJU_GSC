cb4a44d34f491bc058c01598032387bec6483aa5
Azuki.I <azuking69@hotmail.co.jp>
2026-02-04 12:07:15 +0900

    26.02.04 count15

--- 2025_2/course-js/practice/260204.js
+++ 2025_2/course-js/practice/260204.js
@@ -1,15 +1,6 @@
 let myList = [10, 20, 30];
 
-let newList = myList.map((v) => v * 10);
-console.log(myList, newList);
-
-function map(argList, argFn) {
-  let newList = [];
-  for (index in argList) {
-    argList[index] = argFn(argList[index]);
-  }
-  return newList;
-}
-
-// map(myList, (v) => v * 10);
-// console.log(myList); // 100, 200, 300
+myList.forEach((v) => {
+  console.log("-----------");
+  console.log(v);
+});
