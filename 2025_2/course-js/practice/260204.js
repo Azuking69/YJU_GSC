7a67f8c44b21cfa86617ae70d39cfd4621b81926
Azuki.I <azuking69@hotmail.co.jp>
2026-02-04 11:58:48 +0900

    26.02.04 count13

--- 2025_2/course-js/practice/260204.js
+++ 2025_2/course-js/practice/260204.js
@@ -1,10 +1,13 @@
 let myList = [10, 20, 30];
 
-function map(argList, argFn) {
-  for (index in argList) {
-    argList[index] = argFn(argList[index]);
-  }
-}
+let newList = myList.map((v) => v * 10);
+console.log(myList, newList);
 
-map(myList, (v) => v * 10);
-console.log(myList); // 100, 200, 300
+// function map(argList, argFn) {
+//   for (index in argList) {
+//     argList[index] = argFn(argList[index]);
+//   }
+// }
+
+// map(myList, (v) => v * 10);
+// console.log(myList); // 100, 200, 300
