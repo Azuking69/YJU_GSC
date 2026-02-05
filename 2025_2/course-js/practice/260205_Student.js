b660104193e930458e105aa729b91707af16f595
Azuki.I <azuking69@hotmail.co.jp>
2026-02-05 11:14:56 +0900

    26.02.05 count8

--- 2025_2/course-js/practice/260205_Student.js
+++ 2025_2/course-js/practice/260205_Student.js
@@ -1,4 +1,4 @@
-print = console.log;
+let print = console.log;
 // Getter / Setter -> a method to get or set avalue
 //                    to the member variable(praivate)
 
@@ -10,10 +10,22 @@ class Student {
     this.#name = argName;
     this.#age = argAge;
   }
+
+  get name() {
+    return this.#name;
+  }
+  get age() {
+    return this.#age;
+  }
+  set name(argName) {
+    this.#name = argName + "Hello";
+  }
+  set age(argAge) {
+    this.#age = `Age: ${argAge}`;
+  }
 }
 
 let std1 = new Student("gsc", 20);
-print(std1.getName(), std1.getAge()); // OK
-
-std1.#name = 20; // Error
-std1.#age = 20; // Error
+print(std1.name, std1.age); // OK
+std1.name = "YJU";
+print(std1.name, std1.age); // OK
