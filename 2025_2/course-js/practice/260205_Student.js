61b205192a2b7ff0be8e5ce5c56079abec17e6b2
Azuki.I <azuking69@hotmail.co.jp>
2026-02-05 10:55:13 +0900

    26.02.05 count6

--- 2025_2/course-js/practice/260205_Student.js
+++ 2025_2/course-js/practice/260205_Student.js
@@ -9,31 +9,18 @@ class Student {
     this.#name = argName;
     this.#age = argAge;
   }
-
   // getter for name MV
   getName() {
-    return this.#name;
-  }
-  // setter for name MV
-  setName(argName) {
-    this.#name = argName;
+    return `Hello ${this.#name}`;
   }
   // getter for age
   getAge() {
-    return this.#age;
-  }
-  // setter for age
-  setAge(argAge) {
-    if (argAge >= 0 && argAge <= 150) {
-      this.#age = argAge;
-      return true;
-    }
+    return `Age: ${this.#age}`;
   }
 }
 
-let std1 = new Student();
-std1.setName = "GSC";
-print(std1.getName());
+let std1 = new Student("gsc", 20);
+print(std1.getName(), std1.getAge()); // OK
 
-std1.age = 20;
-print(std1.age);
+std1.#name = 20; // Error
+std1.#age = 20; // Error
