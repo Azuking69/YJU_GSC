25a2f69902e4d8f50623afea0ed51d8d1b24e1b6
Azuki.I <azuking69@hotmail.co.jp>
2026-02-05 10:36:11 +0900

    26.02.05 count3

--- 2025_2/course-js/practice/260205_Student.js
+++ 2025_2/course-js/practice/260205_Student.js
@@ -1,9 +1,24 @@
 print = console.log;
+// Getter / Setter -> a method to get or set avalue
+//                    to the member variable(praivate)
 
 class Student {
   #name = undefined;
   #age = undefined;
+
+  // getter for name MV
+  getName() {
+    return this.#name;
+  }
+  // setter for name MV
+  setName(argName) {
+    this.#name = argName;
+  }
 }
 
 let std1 = new Student();
-std1.#name = "GSC";
+std1.setName = "GSC";
+print(std1.getName());
+
+std1.age = 20;
+print(std1.age);
