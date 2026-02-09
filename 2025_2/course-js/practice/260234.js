3231f69440c95fe71b50c4ab5ecc287473760dc6
Azuki.I <azuking69@hotmail.co.jp>
2026-02-09 10:36:36 +0900

    26.02.09 count1

---
+++ 2025_2/course-js/practice/260234.js
@@ -0,0 +1,11 @@
+class Student {
+  name = undefined;
+  age = undefined;
+  gender = undefined;
+
+  constructor(argName, argAge, argGender) {
+    this.name = argName;
+    this.age = argAge;
+    this.gender = argGender;
+  }
+}
