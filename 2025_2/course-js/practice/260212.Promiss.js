4f2c6d3f18c21973492981c6e8057af60c8f56b8
Azuki.I <azuking69@hotmail.co.jp>
2026-02-12 11:53:14 +0900

    26.02.12 count3

--- 2025_2/course-js/practice/260212.Promiss.js
+++ 2025_2/course-js/practice/260212.Promiss.js
@@ -1,10 +1,12 @@
 let p = new Promise(
-    // Excutor
+  // Excutor: 비동기로 실행 -> user defined function
+  () => {},
 );
+// 프로미스 겍체 생성 후 사용자로부터 받은 함수를 실행한다.
 
-p.then(/* OnResolved */, /* OnRejectted */);
+// p.then(/* OnResolved */, /* OnRejectted */);
 
-let p2 = new Promise();
+// let p2 = new Promise();
 
-// Task A -> Task B -> Task C -> Task D
-p2.then().then().then().catch().finally();
+// // Task A -> Task B -> Task C -> Task D
+// p2.then().then().then().catch().finally();
