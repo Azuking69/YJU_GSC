0484205d40ad554fc94cf02b77083750a5847620
Azuki.I <azuking69@hotmail.co.jp>
2026-02-12 12:11:14 +0900

    26.02.12 count4

--- 2025_2/course-js/practice/260212.Promiss.js
+++ 2025_2/course-js/practice/260212.Promiss.js
@@ -1,6 +1,15 @@
 let p = new Promise(
   // Excutor: 비동기로 실행 -> user defined function
-  () => {},
+  () => {
+    console.log("test");
+    a("S");
+    // b("F");
+  },
+);
+
+p.then(
+  (result) => console.log(result),
+  (error) => console.log(error),
 );
 // 프로미스 겍체 생성 후 사용자로부터 받은 함수를 실행한다.
 
