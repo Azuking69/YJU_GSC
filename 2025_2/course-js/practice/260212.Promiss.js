e0cf92e977941848fff1afaa77868b3acb9f7d04
Azuki.I <azuking69@hotmail.co.jp>
2026-02-12 12:38:46 +0900

    26.02.12 count5

--- 2025_2/course-js/practice/260212.Promiss.js
+++ 2025_2/course-js/practice/260212.Promiss.js
@@ -1,9 +1,18 @@
+function makeDelay(argMs) {
+  let endTime = Date.now() + argMs;
+  while (Date.now() < endTime) {}
+  return Math.random() > 0.5;
+}
+
 let p = new Promise(
-  // Excutor: 비동기로 실행 -> user defined function
-  () => {
-    console.log("test");
-    a("S");
-    // b("F");
+  // executor -> user defined function
+  (resolve, reject) => {
+    console.log("start");
+
+    if (makeDelay(3000)) resolve("S");
+    else reject("F");
+
+    console.log("end");
   },
 );
 
@@ -11,11 +20,3 @@ p.then(
   (result) => console.log(result),
   (error) => console.log(error),
 );
-// 프로미스 겍체 생성 후 사용자로부터 받은 함수를 실행한다.
-
-// p.then(/* OnResolved */, /* OnRejectted */);
-
-// let p2 = new Promise();
-
-// // Task A -> Task B -> Task C -> Task D
-// p2.then().then().then().catch().finally();
