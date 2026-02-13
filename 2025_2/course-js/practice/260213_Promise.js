26a65ddb755ad4c84aea969e7601e495bef76400
Azuki.I <azuking69@hotmail.co.jp>
2026-02-13 12:29:12 +0900

    26.02.13 count6

--- 2025_2/course-js/practice/260213_Promise.js
+++ 2025_2/course-js/practice/260213_Promise.js
@@ -1,9 +1,16 @@
-try {
-  console.log(start);
-
-  // throw new Error("My Error");
-} catch (exception) {
-  console.log(`Exception: ${exception}`);
-} finally {
-  console.log("finally");
-}
+new Promise((resolve, reject) => {
+  resolve("Executor: S");
+  // reject("Excutor: F");
+})
+  .then(
+    (result) => {
+      console.log(`then-S: ${result}`);
+    },
+    (error) => {
+      console.log(`then-T: ${error}`);
+      throw new Error("Error!!!! Then");
+    },
+  )
+  //   Exception Handling
+  .catch((error) => console.log(`catch: ${error}`))
+  .finally((result) => console.log(`finally: ${result}`));
