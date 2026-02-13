1162bfefd9ed2933031b791a4b62483b8e39ac98
Azuki.I <azuking69@hotmail.co.jp>
2026-02-13 12:07:39 +0900

    26.02.13 count4

--- 2025_2/course-js/practice/260213_Promise.js
+++ 2025_2/course-js/practice/260213_Promise.js
@@ -1,20 +1,7 @@
-let p1 = new Promise(
-  // Excutor function
-  (resovle, reject) => {
-    console.log("Excutor function is invoked!!");
-    setTimeout(() => console.log("wait 3 seconds!!"), 3000);
-    // resovle(hello);
-    resovle("Hello");
-  },
-);
-
-// p1 -> When Sucess: (result)
-// p1 -> When fail: (error)
-p1.then(
-  (result) => {
-    console.log("S");
-  },
-  (error) => {
-    console.log("F");
-  },
-);
+new Promise((resolve, reject) => {})
+  .then(
+    (result) => {},
+    (error) => {},
+  )
+  .catch((error) => console.log(`catch: ${error}`))
+  .finally((result) => console.log(`finally: ${result}`));
