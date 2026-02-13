efecb68b19f3f3f136d9e9e01a302e158bb26546
Azuki.I <azuking69@hotmail.co.jp>
2026-02-13 12:38:24 +0900

    26.02.13 count9

--- 2025_2/course-js/practice/260213_Promise.js
+++ 2025_2/course-js/practice/260213_Promise.js
@@ -1,4 +1,12 @@
-new Promise((resolve, reject) => {});
+new Promise((resolve, reject) => {
+  resolve("resolve!"); // reject("reject")
+});
 
-let p2 = p1.then();
-console.log(p2 instanceof Promise);
+let p2 = p1.then(
+  (result) => {
+    console.log("p1 - resolve");
+  },
+  (error) => {
+    console.log("p1 - resolve");
+  },
+);
