d10049a45f878ad118f09f8264d8e36c13bc9a6a
Azuki.I <azuking69@hotmail.co.jp>
2026-02-13 10:44:42 +0900

    26.02.13 count1

---
+++ 2025_2/course-js/practice/260213_Promise.js
@@ -0,0 +1,17 @@
+new Promise(
+  // Excutor function
+  (resovle, reject) => {
+    console.log("Excutor function is invoked!!");
+  },
+);
+
+// p1 -> When Sucess: (result)
+// p1 -> When fail: (error)
+p1.then(
+  (result) => {
+    console.log("S");
+  },
+  (error) => {
+    console.log("F");
+  },
+);
