8faac0414d92318bab966efa3e878d4b70b4e3bb
Azuki.I <azuking69@hotmail.co.jp>
2026-05-06 10:46:52 +0900

    26.05.06 count1

---
+++ 2026_1/ML_Theory-Practice/course-ml-basic/class/260506.py
@@ -0,0 +1,20 @@
+
+epochs = 1
+
+# Loop: epoch
+for epoch in range(1, epochs + 1):
+  # Loop: sample
+  for x, y in range(zip()):
+    # 예측값
+    
+    # error 계산: 예측값 - 정답
+    
+    # 각 파라미터의 기울기 계산 후 누적
+    # w -> 2 * Error * x
+    # b -> 2 * Error
+  
+  # cost값 계산 / 평균
+  
+  # optimizer: 파라미터 update
+  
+  # loss값 출력
