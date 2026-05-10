ba3a20ecd9c46e4eb35ed4513b3235a85cd8d141
Azuki.I <azuking69@hotmail.co.jp>
2026-05-10 22:18:31 +0900

    26.05.10 count5

--- 2026_1/ML_Theory-Practice/course-ml-basic/class/practice/linear_warmup.py
+++ 2026_1/ML_Theory-Practice/course-ml-basic/class/practice/linear_warmup.py
@@ -11,27 +11,26 @@ data_size = len(x_data)
 # ============================================================
 # 학생 구현 영역
 # ============================================================
-# HyperParameter setting
+# Hyperparameter setting
 epochs = 500
 batch_size = 4
 base_lr = 0.007
 warmup_steps = 250
 
-# Parameter setting
+# Parameter settting
 w = 0.0
 b = 0.0
 
-# global steps: 1부터 시작
+# global step setting
 global_steps = 1
 
 # Loop: epoch
-# 1 epoch마다 step 수 = data_size / batch_size = 20 / 4 = 5
 for loop_epoch in range(1, epochs + 1):
-    # 매 epoch마다 데이터 순서 섞기
+    # epoch마다 data 순서 섞기
     random_data = list(range(data_size))
     random.shuffle(random_data)
 
-    # epoch loss 초기화
+    # epoch loss를 초기화
     epoch_loss = 0.0
 
 
@@ -39,7 +38,6 @@ for loop_epoch in range(1, epochs + 1):
     for loop_batch in range(0, data_size, batch_size):
         # batch단위로 index를 가져오기
         batch_index = random_data[loop_batch:loop_batch + batch_size]
-
         # batch마다 gradient, loss를 초기화
         grad_w = 0.0
         grad_b = 0.0
@@ -48,12 +46,12 @@ for loop_epoch in range(1, epochs + 1):
 
         # Loop: sample
         for loop_sample in batch_index:
-            # data 가져오기
+            # 데이터 가져오기
             x = x_data[loop_sample]
             y = y_data[loop_sample]
 
             # 예측값 계산
-            # Model: H(X) wx + b
+            # Model: H(X) = wx + b
             pred_y = w * x + b
 
             # error: 예측값 - 정답
@@ -64,13 +62,14 @@ for loop_epoch in range(1, epochs + 1):
             grad_b += 2 * error
             loss += error ** 2
 
+
         # 평균 계산
         batch_len = len(batch_index)
-        grad_w = grad_w / batch_len
-        grad_b = grad_b / batch_len
-        loss = loss / batch_len
+        grad_w /= batch_len
+        grad_b /= batch_len
+        loss /= batch_len
 
-        # Warm up
+        # Warmup 수식
         lr = base_lr * min(1.0, global_steps / warmup_steps)
 
         # Parameter update
@@ -82,14 +81,14 @@ for loop_epoch in range(1, epochs + 1):
 
         # batch마다 + 1
         global_steps += 1
-    
+
+
     # epoch loss 평균
-    epoch_loss = epoch_loss / batch_size
+    epoch_loss /= batch_size
 
     # loss 출력
     if loop_epoch == 1 or loop_epoch % 100 == 0:
         print(f"Epoch: {loop_epoch} | Loss: {epoch_loss:.4f} | w: {w:.4f} | b: {b:.4f}")
 
-
 # 최종 결과 출력
-print(f"\nFinal Loss: {epoch_loss} | Final w: {w:.4f} | Final b: {b:.4f} | Final global steps: {global_steps}")
+print(f"\nFinal loss: {epoch_loss:.4f} | Final w: {w:.4f} | Final b: {b:.4f} | Final global steps: {global_steps}")
