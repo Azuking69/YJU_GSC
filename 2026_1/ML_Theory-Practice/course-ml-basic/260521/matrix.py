bdb2eb6de8bd01ca50410d022326b34d53b4f982
Azuki.I <azuking69@hotmail.co.jp>
2026-05-21 10:50:21 +0900

    26.05.21 count6

--- 2026_1/ML_Theory-Practice/course-ml-basic/260521/matrix.py
+++ 2026_1/ML_Theory-Practice/course-ml-basic/260521/matrix.py
@@ -1,7 +1,9 @@
-bar = [1, 2,, 3, 4] #vector
-pos = [
-  [1],
-  [2],
-  [3],
-  [4]
-] #matrix
+import numpy as np
+
+samples = np.arange(1, 17).reshape(4, 4)
+W = np.array([0.1, 0.2, 0.3, 0.4]).reshape(-1)
+
+print(samples)
+print(W)
+
+print(samples @ W)
