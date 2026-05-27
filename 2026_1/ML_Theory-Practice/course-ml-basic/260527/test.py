39aee41a874f2c2bf70ee3350170676176108e86
Azuki.I <azuking69@hotmail.co.jp>
2026-05-27 10:30:50 +0900

    26.05.27 count7

--- 2026_1/ML_Theory-Practice/course-ml-basic/260527/test.py
+++ 2026_1/ML_Theory-Practice/course-ml-basic/260527/test.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 foo = [1, 2, 3, 4, 5, 6]  # vector(6, )
-pos = [[1, 2], [3, 4], [5, 6]]  # matrix(3, 2)
 
-bar = np.array([2, 2])
+pos = np.array(foo)
 
-print(bar.dtype)  # float32 -> float64
+print(pos)
+print(pos.shape)
