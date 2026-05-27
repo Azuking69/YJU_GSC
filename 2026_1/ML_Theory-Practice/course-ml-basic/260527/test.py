916f87fd716668d3490377d9d84545358d898e7c
Azuki.I <azuking69@hotmail.co.jp>
2026-05-27 09:56:47 +0900

    26.05.27 count4

--- 2026_1/ML_Theory-Practice/course-ml-basic/260527/test.py
+++ 2026_1/ML_Theory-Practice/course-ml-basic/260527/test.py
@@ -1,10 +1,6 @@
 import numpy as np
 
 # 2 x 1 x 3
-bar = np.array([[[3, 4, 20]], [[1, 2, 30]]])
+bar = np.array([2.0, 3.0], dtype=np.float32)
 
-
-print(type(bar))
-print(bar.ndim)
-print(bar.shape)
-print(bar.shape[0], bar.shape[1], bar.shape[2])
+print(bar.dtype)
