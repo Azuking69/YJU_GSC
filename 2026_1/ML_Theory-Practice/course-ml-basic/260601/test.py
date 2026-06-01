df1eb2dc7772fb333a21975b01180fced6c5f8f9
Azuki.I <azuking69@hotmail.co.jp>
2026-06-01 10:30:21 +0900

    26.06.01 count4

--- 2026_1/ML_Theory-Practice/course-ml-basic/260601/test.py
+++ 2026_1/ML_Theory-Practice/course-ml-basic/260601/test.py
@@ -3,11 +3,7 @@ import numpy as np
 # x_data
 x_data = np.arange(1, 26).reshape(5, 5)
 
-# indexing
-# Python - slice indexing
-# start:stop:step
-# start, stop 생략 시 -> 전체
-# stop 생략 시 -> 끝
-# step 생략 시 -> +1
-print(x_data[::1, ::])
-print(x_data[1:-1, 1:-1])
+# 샘플이 랜덤......
+x_data = (np.random.random(12) - 0.5).reshape(3, 4) * 4  # -0.5 ~ 0.5
+
+print(x_data)
