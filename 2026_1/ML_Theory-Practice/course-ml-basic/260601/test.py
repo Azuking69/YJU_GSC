cd4b26095b3a87d8743616d24482ad8a3628b629
Azuki.I <azuking69@hotmail.co.jp>
2026-06-01 10:46:44 +0900

    26.06.01 count6

--- 2026_1/ML_Theory-Practice/course-ml-basic/260601/test.py
+++ 2026_1/ML_Theory-Practice/course-ml-basic/260601/test.py
@@ -1,11 +1,10 @@
 import numpy as np
 
 # x_data
-# x_data = (np.random.random((4, 4)) - 0.5) * 8
+# x_data = (np.random.random((4, 4)) - 0.5) * 8 # (4, 4)
 
-bar = np.arange(1, 7).reshape(2, 3)
+bar = np.arange(1, 4)
+foo = np.arange(4, 7)
 
-print(bar + 2)
-print()
-
-print(bar**2)
+print(f"bar: {bar}\nfoo: {foo}")
+print(f"bar + foo: {bar + foo}")
