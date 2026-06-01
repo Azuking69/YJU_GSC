c51b693e9015f7404ac79afbd1c8edca63cea060
Azuki.I <azuking69@hotmail.co.jp>
2026-06-01 10:42:52 +0900

    26.06.01 count5

--- 2026_1/ML_Theory-Practice/course-ml-basic/260601/test.py
+++ 2026_1/ML_Theory-Practice/course-ml-basic/260601/test.py
@@ -1,9 +1,11 @@
 import numpy as np
 
 # x_data
-x_data = np.arange(1, 26).reshape(5, 5)
+# x_data = (np.random.random((4, 4)) - 0.5) * 8
 
-# 샘플이 랜덤......
-x_data = (np.random.random(12) - 0.5).reshape(3, 4) * 4  # -0.5 ~ 0.5
+bar = np.arange(1, 7).reshape(2, 3)
 
-print(x_data)
+print(bar + 2)
+print()
+
+print(bar**2)
