427a5a5e18f297ffeb8bd1673e782149dea14dcb
Azuki.I <azuking69@hotmail.co.jp>
2026-06-01 09:23:50 +0900

    26.06.01 count2

--- 2026_1/ML_Theory-Practice/course-ml-basic/260601/test.py
+++ 2026_1/ML_Theory-Practice/course-ml-basic/260601/test.py
@@ -1,5 +1,10 @@
 import numpy as np
 
 bar = np.arange(1, 17)
-print(bar)
-print(bar.shape)
+pos = bar.reshape(4, 4)
+foo = pos.reshape(2, 4, 2)
+print(f"bar: {bar}")
+print("*" * 20)
+print(f"pos: {pos}")
+print("*" * 20)
+print(f"foo: {foo}")
