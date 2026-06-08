e4a6f15c013fcce1579e38243a1aa8b34e6a4aca
Azuki.I <azuking69@hotmail.co.jp>
2026-06-08 09:40:06 +0900

    26.06.08 count4

--- 2026_1/ML_Theory-Practice/260608/mlr.py
+++ 2026_1/ML_Theory-Practice/260608/mlr.py
@@ -8,6 +8,8 @@ b_true = 2.0
 # x_dara = (np.random.random((5, 4)) - 0.5) * 8 
 
 bar = np.arange(1, 10).reshape(-1, 3)
-foo = bar
-pos = bar + foo
-print(pos)
+foo = np.array([0.1, 0.2, 0.3, 0.4])
+
+print(bar)
+print(foo)
+print(bar + 2)
