e6a912f19caff29bb8c02fd1e1efb6470e94a06a
Azuki.I <azuking69@hotmail.co.jp>
2026-06-15 10:35:53 +0900

    26.06.15 count7

--- 2026_1/ML_Theory-Practice/260615/test.py
+++ 2026_1/ML_Theory-Practice/260615/test.py
@@ -1,11 +1,3 @@
-import numpy as np
+bar = [10, 20, 30, 40, 50, 60]
 
-bar = np.arange(12).reshape(3, 4)
-
-print(f"bar\n{bar}\n")                   # shape(3, 4)
-                                         # shape[1] -> 4
-print(bar.shape[0], bar.shape[1])        # shape[0] -> 3
-
-print(f"axis=1\n{bar.sum(axis=1)}\n")
-print(f"axis=0\n{bar.sum(axis=0)}\n")
-print(f"axis=1\n{bar.sum(axis=1)}\n")
+print(bar[-3:])
