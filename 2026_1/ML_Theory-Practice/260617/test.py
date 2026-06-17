77481b72e4bf1f2ebe93172174f5df8f56f05271
Azuki.I <azuking69@hotmail.co.jp>
2026-06-17 10:35:56 +0900

    26.06.17 count3

--- 2026_1/ML_Theory-Practice/260617/test.py
+++ 2026_1/ML_Theory-Practice/260617/test.py
@@ -14,19 +14,31 @@ w = np.random.random((D, 1)) # D, 1
 b = np.random.random()
 
 # Hyper Parameters
-epochs = 1000
+epochs = 10000
 lr = 0.007
 
 #Loop: Training, BGD
-for epochs in range(1, epochs + 1):
+for epoch in range(1, epochs + 1):
     # prediction
     y_pred = x_data @ w + b # N, 1
     
-    print(y_pred)
+    # error
+    error = y_pred - y_data
+    
+    # grad: w_grad, b_grad
+    w_grad = (2 / N) * x_data.T @ error #D, 1
+    b_grad = (2 / N) * error.sum() # 1
+    
+    # update parameters
+    w = w - lr * w_grad
+    b = b - lr * b_grad
+    
+    if epoch % 1000 == 0:
+      print(f"epoch: {epoch}, Loss: {(error**2).mean():.4f}\n")
     ...
       
-# print(f"w_true: {w_true}\n")
-# print(f"b_true: {b_true}\n")
-# print("*" * 10)
-# print(f"w: {w}\n")
-# print(f"b: {b}\n")
+print(f"w_true: {w_true}\n")
+print(f"b_true: {b_true}\n")
+print("*" * 50)
+print(f"w: {w}\n")
+print(f"b: {b}\n")
