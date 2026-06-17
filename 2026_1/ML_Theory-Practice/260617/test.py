e59b25246c4e46d747f4ddf4d46af5e54fe246ae
Azuki.I <azuking69@hotmail.co.jp>
2026-06-17 09:21:30 +0900

    26.06.17 count1

---
+++ 2026_1/ML_Theory-Practice/260617/test.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+np.random.seed(0)
+N, D = 5, 4
+
+w_true = np.array([0.2, 0.4, 0.5, 0.7]).reshape(-1, 1) # D, 1
+b_true = 2.0 
+
+x_data = (np.random.random((N, D)) - 0.5) * 8 #N, D
+y_data = x_data @ w_true + b_true # N, 1
+
+# Model Parameters
+w = np.random.random((D, 1)) # D, 1
+b = np.random.random()
+
+# Hyper Parameters
+epochs = 1000
+lr = 0.007
+
+#Loop: Training
+for epochs in range(1, epochs + 1):
+    ...
+      
+# print(f"w_true: {w_true}\n")
+# print(f"b_true: {b_true}\n")
+# print("*" * 10)
+# print(f"w: {w}\n")
+# print(f"b: {b}\n")
