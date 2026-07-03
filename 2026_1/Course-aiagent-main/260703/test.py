48d5a6f5c198edd155dd719de467932420726954
Azuki.I <azuking69@hotmail.co.jp>
2026-07-03 15:30:13 +0900

    26.07.03 count5

--- 2026_1/Course-aiagent-main/260703/test.py
+++ 2026_1/Course-aiagent-main/260703/test.py
@@ -1,19 +1,18 @@
 import time
+import asyncio
 
-def make_order(menu:str, taken_time:int)->str:
+async def make_order(menu:str, taken_time:int)->str: # 비동기 함수 정의
     print(f"{menu} 준비중")
-    time.sleep(taken_time)
+    await asyncio.sleep(taken_time) # 비동기 함수 호출, wait
     return f"{menu} 준비 완료"
 
-def main():
+async def main():
     start = time.time()
-    print(make_order("라떼", 3))
-    print(make_order("아아", 2))
-    print(make_order("우유", 1))
-
+    result = await asyncio.create_task(make_order("라떼", 3)) # Task 객체 생성, wait
+    print(result)
     elasped_time = time.time() - start
 
     print(f"경과 시간 {elasped_time}")
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main()) # create event loop
