a2d59c79e75af5382a4cbc9ee32b1b4004394b6c
Azuki.I <azuking69@hotmail.co.jp>
2026-07-03 15:45:13 +0900

    26.07.03 count6

--- 2026_1/Course-aiagent-main/260703/test.py
+++ 2026_1/Course-aiagent-main/260703/test.py
@@ -8,8 +8,15 @@ async def make_order(menu:str, taken_time:int)->str: # 비동기 함수 정의
 
 async def main():
     start = time.time()
-    result = await asyncio.create_task(make_order("라떼", 3)) # Task 객체 생성, wait
-    print(result)
+    result1 = asyncio.create_task(make_order("라떼", 3)) # Task 객체 생성, wait
+    result2 = asyncio.create_task(make_order("아아", 2)) # Task 객체 생성, wait
+    result3 = asyncio.create_task(make_order("우유", 1)) # Task 객체 생성, wait
+    
+    await result1
+    await result2
+    await result3
+    print(result1, result2, result3)
+
     elasped_time = time.time() - start
 
     print(f"경과 시간 {elasped_time}")
