d0fd643782752da5d3d7a0761181e0d052afeff7
Azuki.I <azuking69@hotmail.co.jp>
2026-07-03 15:16:38 +0900

    26.07.03 count4

--- 2026_1/Course-aiagent-main/260703/test.py
+++ 2026_1/Course-aiagent-main/260703/test.py
@@ -1,8 +1,16 @@
 import time
 
+def make_order(menu:str, taken_time:int)->str:
+    print(f"{menu} 준비중")
+    time.sleep(taken_time)
+    return f"{menu} 준비 완료"
+
 def main():
     start = time.time()
-    time.sleep(2) # 2초 정지
+    print(make_order("라떼", 3))
+    print(make_order("아아", 2))
+    print(make_order("우유", 1))
+
     elasped_time = time.time() - start
 
     print(f"경과 시간 {elasped_time}")
