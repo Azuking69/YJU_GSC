ab7eff97f59306401ca401a8370d4fae8375f74b
Azuki.I <azuking69@hotmail.co.jp>
2026-07-06 16:39:13 +0900

    26.07.06 count12

--- 2026_1/Course-aiagent-main/260706/test3.py
+++ 2026_1/Course-aiagent-main/260706/test3.py
@@ -2,12 +2,14 @@ import asyncio
 import time
 
 async def make_order(item, taken_time):
-    print(f"{item} 준비 시작")
+    print(f"{item} 접수")
     await asyncio.sleep(taken_time)
     print(f"{item} 준비완료")
 
 async def main():
-    ...
+    await make_order("라떼", 3)
+    await make_order("아아", 2)
+    await make_order("우유", 1)
 
 if __name__ == "__main__":
     asyncio.run(main()) # EL system start
