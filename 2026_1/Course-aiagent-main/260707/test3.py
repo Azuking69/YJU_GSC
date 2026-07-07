f0130511f1d1703b6264636c2788b9772a4c0221
Azuki.I <azuking69@hotmail.co.jp>
2026-07-07 16:35:05 +0900

    26.07.07 count12

--- 2026_1/Course-aiagent-main/260707/test3.py
+++ 2026_1/Course-aiagent-main/260707/test3.py
@@ -1,6 +1,19 @@
-def test(arg):
-    print(f"arg: {arg}")
+def test(a, b, c, d):
+    print(f"a: {a}")
+    print(f"a: {b}")
+    print(f"a: {c}")
+    print(f"a: {d}")
 
-bar = [test(value) for value in range(1, 6)]
+# bar = [1, 2, 3, 4]
+foo = {'d':10, 'b':2, 'c':4, 'a':1}
+test(**foo)
 
-print(bar)
+
+# def get_sum_avg(a, b):
+#     sum = a + b
+#     avg = sum / 2
+#     return sum, avg
+
+# # unpacking
+# sum, avg = get_sum_avg(1, 2)
+# print(sum, avg)
