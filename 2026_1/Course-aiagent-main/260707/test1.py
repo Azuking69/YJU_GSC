29dd4bd88bf42c4358f59de86128b45a3dc0fa4a
Azuki.I <azuking69@hotmail.co.jp>
2026-07-07 15:59:25 +0900

    26.07.07 count8

--- 2026_1/Course-aiagent-main/260707/test1.py
+++ 2026_1/Course-aiagent-main/260707/test1.py
@@ -3,8 +3,8 @@ import time
 from anthropic import AsyncAnthropic
 
 MODEL = "claud-haiku-4-5-20251001"
-async def ask(client: AsyncAnthropic, mas:str)->str:
-    response = await client.massage.create( # await로 비동기 호출
+async def ask(client: AsyncAnthropic, message:str)->str:
+    response = await client.messages.create( # await로 비동기 호출
         model = MODEL,
         max_token = 100,
         message = [{"role": "user", "content": question}],
@@ -17,18 +17,18 @@ async def main():
     start_time = time.time()
     # LLM prompt 전송
     client = AsyncAnthropic()
-
+    text = ["파이썬은 뭐야?", "자바스크립트는 뭐야?", "C# 언어는 뭐야?"]
     result = await asyncio.gather(
-        ask(client, "파이썬은 뭐야?"), # "파이썬은 뭐야?"
-        ask(client, "자바스크립트는 뭐야?"), # "자바스크립트는 뭐야?"
-        ask(client, "GO 언어는 뭐야?"), # "GO 언어는 뭐야?"
+        *[ask(client, value) for value in text]
     )
 
+    # 걸린 시간
     elapsed_time = time.time() - start_time
 
     for text in result:
         print(text)
         print("*" * 50)
+        
     print(f"총 소요시간: {elapsed_time}")
 
 
---
+++ 2026_1/Course-aiagent-main/260707/test2.py
@@ -0,0 +1,4 @@
+# [컴플리현션] # list, string
+bar = [value for value in range(1, 10 + 1, 2)]
+print(bar)
+# {컴플리현션} # dict, set
