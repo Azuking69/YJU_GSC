1f74d6595c60fa515a525bfe78621d9af611d4de
Azuki.I <azuking69@hotmail.co.jp>
2026-07-07 16:03:41 +0900

    26.07.07 count10

--- 2026_1/Course-aiagent-main/260707/test2.py
+++ 2026_1/Course-aiagent-main/260707/test2.py
@@ -3,3 +3,6 @@ bar = [value for value in range(1, 10 + 1, 2)
        if value % 3 == 0]
 print(bar)
 # {컴플리현션} # dict, set
+text = ["a", "abc", "ef"]
+foo = {value:len(value) for value in text}
+print(foo)
