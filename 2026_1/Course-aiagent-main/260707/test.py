d12f6c2aea8397e0ebd98fd16bd45da4ee94249f
Azuki.I <azuking69@hotmail.co.jp>
2026-07-07 14:59:30 +0900

    26.07.07 count5

--- 2026_1/Course-aiagent-main/260707/test.py
+++ 2026_1/Course-aiagent-main/260707/test.py
@@ -12,8 +12,8 @@ async def main():
     result2 = asyncio.create_task(make_order("아아", 2))
     result3 = asyncio.create_task(make_order("우유", 1))
     
-    elapsed_time = time.time() - start_time
     await result1, result2, result3
+    elapsed_time = time.time() - start_time
     print(f"총 소요시간: {elapsed_time}")
 
 
