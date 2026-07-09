092c63cf60479d9ac685afb53e79c8552977fd34
Azuki.I <azuking69@hotmail.co.jp>
2026-07-09 14:13:21 +0900

    26.07.09 count1

---
+++ 2026_1/Course-aiagent-main/260709/test.py
@@ -0,0 +1,13 @@
+import asyncio
+import time
+from anthropic import Anthropic
+
+asysnc def call_llm():
+    ...
+
+async def main():
+    # LLM 호출
+    ...
+
+if __name__ == "__main__":
+    asyncio.run(main())
