a6572ec3b1d704fe9dee5a0a82177c09988e31b3
Azuki.I <azuking69@hotmail.co.jp>
2026-07-21 16:40:32 +0900

    26.07.21 count3

--- 2026_1/Course-aiagent-main/260721/test.py
+++ 2026_1/Course-aiagent-main/260721/test.py
@@ -1,11 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validationError
 from typing import Literal
 
+# value -> validation -> object
 class car(BaseModel):
     model: str
     year: int
     c_type: Literal[ "suv", "sedan", "pickup"]
 
-bar = car(model="x7", year=2026, c_type="suv")
+try:
+    bar = car(model="x7", year=2026, c_type="truck")
+except validationError:
+    print("데이터 에러")
 
-print(bar.model_dump_json())
+
+# json -> object
+
+# print(bar.model_dump_json())
