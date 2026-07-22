5697d23fd58af86d71312dfba0b092312e299974
Azuki.I <azuking69@hotmail.co.jp>
2026-07-22 16:27:10 +0900

    26.07.22 count5

--- 2026_1/Course-aiagent-main/260722/test1.py
+++ 2026_1/Course-aiagent-main/260722/test1.py
@@ -8,9 +8,11 @@ class Student(BaseModel):
     grade:Literal["1","2","3"]
 
 try:
-    rcvd_data = '{"id": 1, "name": "gsc", "grade": "4"}'
+    rcvd_data = '{"id": 1, "name": "gsc", "grade": "3"}'
     obj = Student.model_validate_json(rcvd_data)
     print(obj)
+    print()
+    print(obj.model_dump_json())
 except ValidationError:
     print("예외처리")
 
