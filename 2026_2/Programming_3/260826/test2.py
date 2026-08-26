16571133801c69c2803cc3fd847a64ed4cff109d
Azuki.I <azuking69@hotmail.co.jp>
2026-08-26 10:25:06 +0900

    26.08.26 count2

--- 2026_2/Programming_3/260826/test2.py
+++ 2026_2/Programming_3/260826/test2.py
@@ -2,12 +2,13 @@
 class Student:
   # 생성자 -> 반환값X
   # 생성자 주소가 복사할 때니까 반환값이 없음
-  def __init__(self, arg_name):
+  def __init__(self, arg_name): # 인스턴스 멤버 변수
     # Object
     self.name = arg_name
     # -> Instantiation
     # Student() -> () : 생성자 호출
   
-
-bar = Student("글시융")
-print(bar.name)
+# Instance of Student class
+std_1 = Student("글시융 1")
+std_2 = Student("글시융 2")
+print(std_1.name, std_2.name)
