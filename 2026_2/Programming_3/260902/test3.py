b8238ed4df5d339cb621aada4308be594f869828
Azuki.I <azuking69@hotmail.co.jp>
2026-09-02 10:01:18 +0900

    26.09.02 count5

--- 2026_2/Programming_3/260902/test3.py
+++ 2026_2/Programming_3/260902/test3.py
@@ -11,3 +11,12 @@ class Student:
         self.name = "홍길동"
         self.id = 1234
         self.gpa = 4.5
+
+    def get_sum(self):
+        self.sum = 20 + 30
+
+
+std_1 = Student()
+std_1.get_sum()
+std_1.univ = "YJU"
+print(std_1.name, std_1.id, std_1.gpa, std_1.sum, std_1.univ)
