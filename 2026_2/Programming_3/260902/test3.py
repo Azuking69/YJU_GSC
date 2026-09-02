16a97097f608cf634480edfaa3313dbfc3e72a17
Azuki.I <azuking69@hotmail.co.jp>
2026-09-02 09:49:46 +0900

    26.09.02 count4

---
+++ 2026_2/Programming_3/260902/test3.py
@@ -0,0 +1,13 @@
+class Student:
+    """
+    학교 -> 공통
+    학과 -> 공통
+    이름
+    학번
+    GPA
+    """
+
+    def __init__(self):
+        self.name = "홍길동"
+        self.id = 1234
+        self.gpa = 4.5
