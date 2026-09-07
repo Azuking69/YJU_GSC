740d4413890c19b0ee4a2941702adb5fa900a2f3
Azuki.I <azuking69@hotmail.co.jp>
2026-09-07 10:35:05 +0900

    26.09.07 count9

--- 2026_2/Programming_3/260907/test3.py
+++ 2026_2/Programming_3/260907/test3.py
@@ -1,10 +1,13 @@
 class Util:
+    cnt_add = 0; cnt_avg = 0
     @classmethod
     def add(cls, *arg_list):
+        cls.cnt_add += 1
         return sum(arg_list)
 
     @classmethod
     def avg(cls, *arg_list):
+        cls.cnt_avg += 1
         return sum(arg_list) / len(arg_list)
 
 print(Util.add(1, 2, 3))
