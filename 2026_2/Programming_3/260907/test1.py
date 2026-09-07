71d683286e5ba92882e00efd1b9951c31b1dcc7c
Azuki.I <azuking69@hotmail.co.jp>
2026-09-07 09:40:37 +0900

    26.09.07 count4

--- 2026_2/Programming_3/260907/test1.py
+++ 2026_2/Programming_3/260907/test1.py
@@ -11,11 +11,18 @@ class Student:
         # 2) 인스턴스 멤버 메소드
 
         # 1) 클래스 멤버 메소드
-        def printInfo(self, s):
-            print(self.id, self.name)
+        @classmethod
+        def printUniv(cls):
+            self.name = "c"
+            print(cls.univ)
+        # def printInfo(self, s):
+        #     print(self.id, self.name)
+            
 
-std_1 = Student("a", 1)
-std_2 = Student("b", 2)
+Student.prtUniv()
 
-print(std_1.id, std_1.name, std_1.univ)
-print(std_2.id, std_2.name, std_2.univ)
+# std_1 = Student("a", 1)
+# std_2 = Student("b", 2)
+
+# print(std_1.id, std_1.name, std_1.univ)
+# print(std_2.id, std_2.name, std_2.univ)
