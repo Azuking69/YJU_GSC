350a274fcda392a0807e916282893900540c38be
Azuki.I <azuking69@hotmail.co.jp>
2026-09-09 09:44:10 +0900

    26.09.09 count3

--- 2026_2/Programming_3/260909/test2.py
+++ 2026_2/Programming_3/260909/test2.py
@@ -1,9 +1,13 @@
-# 각 메소드의 호출 카운트 확인하고 싶다
+# add  메소드의 호출 카운트 확인하고 싶다
 class my_util:
-    @staticmethod
-    def add(a, b):
+    add_clk_cnt = 0
+
+    @classmethod
+    def add(cls, a, b):
+        cls.add_clk_cnt += 1
         return a + b
 
+    @staticmethod
     def avg(a, b):
         return (a + b) / 2
 
