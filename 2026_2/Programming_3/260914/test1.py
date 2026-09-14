1cc34607bb142668c353b9a750cbc422dba83571
Azuki.I <azuking69@hotmail.co.jp>
2026-09-14 09:11:09 +0900

    26.09.14 count2

--- 2026_2/Programming_3/260914/test1.py
+++ 2026_2/Programming_3/260914/test1.py
@@ -1,6 +1,6 @@
 class Foo:
-    def __init__(self):
+    def __init__(self): # 생성자 Constructure
         print("Foo 생성")
 
-    def __del__(self):
+    def __del__(self): # 소멸자 Destructor
         print("Foo 소멸")
