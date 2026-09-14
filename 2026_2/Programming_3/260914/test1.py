71983a1b4dbdd1df883957fab50479acbc1a6d89
Azuki.I <azuking69@hotmail.co.jp>
2026-09-14 09:12:26 +0900

    26.09.14 count3

--- 2026_2/Programming_3/260914/test1.py
+++ 2026_2/Programming_3/260914/test1.py
@@ -2,5 +2,6 @@ class Foo:
     def __init__(self): # 생성자 Constructure
         print("Foo 생성")
 
+    # Magic Method, Magic member variable
     def __del__(self): # 소멸자 Destructor
         print("Foo 소멸")
