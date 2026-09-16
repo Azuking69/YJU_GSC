45ab742c0d852b85d0781897a1bb69d1fe8cbd25
Azuki.I <azuking69@hotmail.co.jp>
2026-09-16 09:58:17 +0900

    26.09.16 count1

---
+++ 2026_2/Programming_3/260916/test1.py
@@ -0,0 +1,16 @@
+class Bar:
+    def __init__(self):
+        self.name = "GSC"
+
+    def prt_info(self):
+        print(self.name, self.age)
+
+
+class Foo(Bar):
+    def __init__(self):
+        self.age = 18
+        super().__init__()
+
+
+obj = Foo()
+obj.prt_info()
