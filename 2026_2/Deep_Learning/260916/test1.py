6e9fac3829656db7cd3352ddecca173181a47e4a
Azuki.I <azuking69@hotmail.co.jp>
2026-09-16 11:37:48 +0900

    26.09.16 count3

--- 2026_2/Deep_Learning/260916/test1.py
+++ 2026_2/Deep_Learning/260916/test1.py
@@ -1,5 +1,10 @@
 from sklearn.datasets import load_breast_cancer
+import numpy as np
 
-ds = datasets.load_bereast_cancer()
+X, y = datasets.load_bereaest_cancer(return_X_y=True)
 
-print(ds.SESCR)
+# print(ds.SESCR)
+# print(f"X: {X.shape}, y: {y.shape}")
+print(X[0, :5])
+print(y[0])
+print(np.count_nonzero(y))
